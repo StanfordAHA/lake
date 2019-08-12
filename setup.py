fc35d1b73c27438447c8bbaa5ec48b6bd6d31a84
mbstrange2 <mstrange@stanford.edu>
2019-08-11 21:19:54 -0700

    cleaning up

--- setup.py
+++ setup.py
@@ -2,14 +2,16 @@ from setuptools import setup
 
 setup(
         name='lake',
-        version='0.1',
-        description='Memory Generator based on Kratos: The God of War.',
-        author='Maxwell Strange',
-        author_email='mstrange@stanford.edu',
         packages=[
             "lake"
         ],
+        version='0.0.1',
+        author='Maxwell Strange',
+        author_email='mstrange@stanford.edu',
+        description='Memory Generator based on Kratos: The God of War.',
+        url="https://github.com/StanfordAHA/lake",
+        python_requires=">=3.5",
         install_requires=[
             "kratos"
-        ],
+        ]
 )
