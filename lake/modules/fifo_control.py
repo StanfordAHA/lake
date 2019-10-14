7fae98ace3b6a574d841aaad878eb69783fe034c
mbstrange2 <mstrange@stanford.edu>
2019-10-13 21:45:28 -0700

    bug for keyi

--- lake/modules/fifo_control.py
+++ lake/modules/fifo_control.py
@@ -2,7 +2,7 @@ from kratos import *
 
 class FIFOControl(Generator):
     def __init__(self, data_width, banks, mem_depth):
-        super().__init__("fifo_control")
+        super().__init__("fifo_control", True)
 
         self.data_width = data_width
         self.banks = banks
@@ -107,11 +107,34 @@ class FIFOControl(Generator):
         self.wire(self._fifo_to_mem_addr, self._data_addr)
         self.wire(self._data_out_sel, self._mem_to_fifo_data)
 
+        self.data_out_comb = self.combinational()
+        self.data_out_if = self.data_out_comb.if_(self._ren_mem_reg[0] & (~self._empty_d1 | self._write_d1))
+        self.data_out_if.add_then_stmt(self._data_out.assign(ternary(self._passthru, self._passthru_reg, self._data_out_sel[0])))
+        for i in range(self.banks-1):
+            self.temp_if = IfStmt(self._ren_mem_reg[i] & (~self._empty_d1 | self._write_d1))
+            self.temp_if.add_then_stmt(self._data_out.assign(ternary(self._passthru, self._passthru_reg, self._data_out_sel[i])))
+            self.data_out_if.add_else_stmt(self.temp_if)
+            self.data_out_if = self.temp_if
+        self.data_out_if.add_else_stmt(self._data_out.assign(self._data_out_reg))
 
         self.add_code(self.empty_d1_reg)
         self.add_code(self.write_d1_reg)
         self.add_code(self.valid_update)
         self.add_code(self.num_words_update)
+        self.add_code(self.next_num_words_comb)
+        self.add_code(self.data_addr_data_out_comb)
+        self.add_code(self.read_addr_update)
+        self.add_code(self.write_addr_update)
+        self.add_code(self.init_stage_update)
+        self.add_code(self.read_to_write_stage_update)
+        self.add_code(self.data_out_reg_update)
+        self.add_code(self.passthru_update)
+        self.add_code(self.passthru_reg_update)
+        self.add_code(self.write_buffed_update)
+        self.add_code(self.write_buff_update)
+        self.add_code(self.write_buff_addr_update)
+        self.add_code(self.ren_mem_reg_update)
+
         ##### GENERATION LOGIC: end
 
     @always((posedge, "clk"), (posedge, "reset"))
@@ -155,7 +178,173 @@ class FIFOControl(Generator):
         else:
             self._next_num_words_mem = 0
 
+    def data_addr_data_out_comb(self):
+        for i in range(self.banks):
+            self._data_addr[i] = ternary(self._write_buffed[i], self._write_buff_addr[i], ternary(self._ren_mem[i], self._read_addr_mem, self._write_addr_mem))
+    
+    @always((posedge, "clk"), (posedge, "reset"))
+    def read_addr_update(self):
+        if self._reset:
+            self._read_addr = 0
+        elif self._clk_en:
+            if self._flush:
+                self._read_addr = 0
+            # READ AND NO WRITE
+            elif self._ren & ~self._wen:
+                if self._circular_en & ~self._empty:
+                    if (self._read_addr + 1) == self._write_addr:
+                        self._read_addr = 0
+                    else:
+                        self._read_addr = self._read_addr + 1
+                elif ~self._empty:
+                    self._read_addr = self._read_addr + 1
+            # READ AND WRITE
+            elif self._ren & self._wen:
+                self._read_addr = self._read_addr + 1
+
+    @always((posedge, "clk"), (posedge, "reset"))
+    def write_addr_update(self):
+        if self._reset:
+            self._write_addr = 0
+        elif self._clk_en:
+            if self._flush:
+                self._write_addr = 0
+            # WRITE AND NO READ AND NOT FULL
+            elif self._wen & ~self._ren & ~self._full:
+                self._write_addr = self._write_addr + 1
+            # READ AND WRITE
+            elif self._ren & self._wen:
+                self._write_addr = self._write_addr + 1
+
+    @always((posedge, "clk"), (posedge, "reset"))
+    def init_stage_update(self):
+        if self._reset:
+            self._init_stage = 1
+        elif self._clk_en:
+            if self._flush:
+                self._init_stage = 1
+            # Transition out of the init stage after a read or write
+            elif self._wen | self._ren:
+                self._init_stage = 0
+
+    @always((posedge, "clk"), (posedge, "reset"))
+    def read_to_write_stage_update(self):
+        if self._reset:
+            self._read_to_write = 1
+        elif self._clk_en:
+            if self._flush:
+                self._read_to_write = 1
+            # Transition out of the init stage after a read or write
+            # READ AND NO WRITE
+            elif self._ren & ~self._wen:
+                if self._circular_en & ~self._empty:
+                    if (self._read_addr + 1) == self._write_addr:
+                        self._read_to_write = 1
+                    else:
+                        self._read_to_write = 0
+                elif ~self._empty:
+                    if (self._read_addr + 1) == self._write_addr:
+                        self._read_to_write = 1
+                    else:
+                        self._read_to_write = 0
+            # WRITE AND NO READ
+            elif self._wen & ~self._ren & ~self._full:
+                self._read_to_write = 0
+
+    @always((posedge, "clk"), (posedge, "reset"))
+    def data_out_reg_update(self):
+        if self._reset:
+            self._data_out_reg = 0
+        elif self._clk_en:
+            if self._flush:
+                self._data_out_reg = 0
+            # Transition out of the init stage after a read or write
+            else:
+                self._data_out_reg = self._data_out
+
+    @always((posedge, "clk"), (posedge, "reset"))
+    def passthru_update(self):
+        if self._reset:
+            self._passthru = 0
+        elif self._clk_en:
+            if self._flush:
+                self._passthru = 0
+            # Transition out of the init stage after a read or write
+            elif self._ren & ~self._wen & ~self._empty:
+                self._passthru = 0
+            elif self._wen & ~self._ren & ~self._full:
+                self._passthru = 0
+            elif self._ren & self._wen:
+                if self._empty & self._same_bank.r_or():
+                    self._passthru = 1
+                else:
+                    self._passthru = 0
+
+    @always((posedge, "clk"), (posedge, "reset"))
+    def passthru_reg_update(self):
+        if self._reset:
+            self._passthru_reg = 0
+        elif self._clk_en:
+            if self._flush:
+                self._passthru_reg = 0
+            # Transition out of the init stage after a read or write
+            elif self._ren & self._wen:
+                if self._empty & self._same_bank.r_or():
+                    self._passthru_reg = self._data_in
+
+    @always((posedge, "clk"), (posedge, "reset"))
+    def write_buffed_update(self):
+        if self._reset:
+            self._write_buffed = 0
+        elif self._clk_en:
+            if self._flush:
+                self._write_buffed = 0
+            else:
+                # We know that it's 1 hot
+                for i in range(self.banks):
+                    if self._same_bank[i]:
+                        self._write_buffed[i] = 1
+                    elif self._write_buffed[i]:
+                        self._write_buffed[i] = 0
+
+    @always((posedge, "clk"), (posedge, "reset"))
+    def write_buff_update(self):
+        if self._reset:
+            self._write_buff = 0
+        elif self._clk_en:
+            if self._flush:
+                self._write_buff = 0
+            else:
+                # We know that it's 1 hot
+                for i in range(self.banks):
+                    if self._same_bank[i]:
+                        self._write_buff[i] = self._data_in
+
+    @always((posedge, "clk"), (posedge, "reset"))
+    def write_buff_addr_update(self):
+        if self._reset:
+            self._write_buff_addr = 0
+        elif self._clk_en:
+            if self._flush:
+                self._write_buff_addr = 0
+            else:
+                # We know that it's 1 hot
+                for i in range(self.banks):
+                    if self._same_bank[i]:
+                        self._write_buff_addr[i] = self._data_in
+
+    @always((posedge, "clk"), (posedge, "reset"))
+    def ren_mem_reg_update(self):
+        if self._reset:
+            self._ren_mem_reg = 0
+        elif self._clk_en:
+            if self._flush:
+                self._ren_mem_reg = 0
+            # Transition out of the init stage after a read or write
+            else:
+                for i in range(self.banks):
+                    self._ren_mem_reg[i] = self._ren_mem[i]
 
 if __name__ == "__main__":
-    fifo_dut = FIFOControl()
+    fifo_dut = FIFOControl(16, 2, 512)
     verilog(fifo_dut, filename="fifo_control.sv", check_active_high=False)
