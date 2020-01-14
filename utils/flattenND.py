4e8bf18302ee9043b39ff198c58699e331d5b996
Kavya Sreedhar <skavya@kiwi.stanford.edu>
2020-01-13 16:38:48 -0800

    ND

---
+++ utils/flattenND.py
@@ -0,0 +1,23 @@
+import kratos
+from kratos import *
+
+class FlattenND(Generator):
+    def __init__(self, array_width, *array_size):
+        super().__init__("flattenND", True)
+
+        total_dims = array_width * array_size[0]
+        apparent_width = array_width
+        for i in range(1, len(array_size)):
+            total_dims = total_dims*array_size[i]
+            apparent_width = apparent_width*array_size[i]
+
+        self.in_ = self.input("in", width=array_width, size=array_size)
+        self.out_ = self.output("out", width=total_dims)
+
+        self.add_code(self.flatten_array)
+
+    @always_comb
+    def flatten_array(self):
+        for i in range(array_size[0]):
+            self.out_[(i+1)*apparent_width - 1, i*apparent_width] = self.in_[i]
+
