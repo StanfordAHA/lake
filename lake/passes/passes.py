025f9d79edd231e43b102941af9b80678fffd697
Keyi Zhang <keyi@stanford.edu>
2020-04-01 14:49:35 -0700

    simplify other passes as well

--- lake/passes/passes.py
+++ lake/passes/passes.py
@@ -17,34 +17,30 @@ def lift_config_reg(generator):
                 ports_ = node.get_port_names()
                 for port_name in ports_:
                     curr_port = node.get_port(port_name)
-                    if(len(curr_port.get_attributes()) <= 0):
+                    attrs = curr_port.find_attribute(lambda a: isinstance(a, ConfigRegAttr))
+                    if len(attrs) != 1:
                         continue
-                    else:
-                        cr_attr = None
-                        doc = None
-                        for i in range(len(curr_port.get_attributes())):
-                            if(isinstance(curr_port.get_attributes()[i].get(), ConfigRegAttr)):
-                                cr_attr = curr_port.get_attributes()[i].get()
-                        if cr_attr:
-                            doc = cr_attr.get_documentation()
-                            # need to wire it to the instances parent and up
-                            gen = node
-                            parent_gen = gen.parent_generator()
-                            child_port = curr_port
-                            child_gen = gen
-                            while parent_gen is not None:
-                                # create a port based on the target's definition
-                                new_name = child_gen.instance_name + "_" + child_port.name
-                                p = parent_gen.port(child_port, new_name)
-                                parent_gen.wire(child_port, p)
-                                # move up the hierarchy
-                                child_port = p
-                                child_gen = parent_gen
-                                parent_gen = parent_gen.parent_generator()
 
-                            child_port_cra = ConfigRegAttr()
-                            child_port_cra.set_documentation(doc)
-                            child_port.add_attribute(child_port_cra)
+                    cr_attr = attrs[0]
+                    doc = cr_attr.get_documentation()
+                    # need to wire it to the instances parent and up
+                    gen = node
+                    parent_gen = gen.parent_generator()
+                    child_port = curr_port
+                    child_gen = gen
+                    while parent_gen is not None:
+                        # create a port based on the target's definition
+                        new_name = child_gen.instance_name + "_" + child_port.name
+                        p = parent_gen.port(child_port, new_name)
+                        parent_gen.wire(child_port, p)
+                        # move up the hierarchy
+                        child_port = p
+                        child_gen = parent_gen
+                        parent_gen = parent_gen.parent_generator()
+
+                    child_port_cra = ConfigRegAttr()
+                    child_port_cra.set_documentation(doc)
+                    child_port.add_attribute(child_port_cra)
 
     v = ConfigRegLiftVisitor()
     v.visit_root(generator)
@@ -67,25 +63,25 @@ def change_sram_port_names(use_sram_stub, sram_macro_info):
 
             def visit(self, node):
                 if isinstance(node, Port):
-                    if not(len(node.get_attributes()) <= 0 or self.use_sram_stub):
-                        for i in range(len(node.get_attributes())):
-                            if (isinstance(node.get_attributes()[i].get(), SRAMPortAttr)):
-                                if node.name == "sram_addr":
-                                    node.name = self.sram_macro_info.addr_port_name
-                                elif node.name == "sram_cen":
-                                    node.name = self.sram_macro_info.ce_port_name
-                                elif node.name == "sram_clk":
-                                    node.name = self.sram_macro_info.clk_port_name
-                                elif node.name == "sram_data_in":
-                                    node.name = self.sram_macro_info.data_in_port_name
-                                elif node.name == "sram_data_out":
-                                    node.name = self.sram_macro_info.data_out_port_name
-                                elif node.name == "sram_wen":
-                                    node.name = self.sram_macro_info.wen_port_name
-                                elif node.name == "sram_wtsel":
-                                    node.name = self.sram_macro_info.wtsel_port_name
-                                elif node.name == "sram_rtsel":
-                                    node.name = self.sram_macro_info.rtsel_port_name
+                    attrs = node.find_attribute(lambda a: isinstance(a, SRAMPortAttr))
+                    if len(attrs) != 1:
+                        return
+                    if node.name == "sram_addr":
+                        node.name = self.sram_macro_info.addr_port_name
+                    elif node.name == "sram_cen":
+                        node.name = self.sram_macro_info.ce_port_name
+                    elif node.name == "sram_clk":
+                        node.name = self.sram_macro_info.clk_port_name
+                    elif node.name == "sram_data_in":
+                        node.name = self.sram_macro_info.data_in_port_name
+                    elif node.name == "sram_data_out":
+                        node.name = self.sram_macro_info.data_out_port_name
+                    elif node.name == "sram_wen":
+                        node.name = self.sram_macro_info.wen_port_name
+                    elif node.name == "sram_wtsel":
+                        node.name = self.sram_macro_info.wtsel_port_name
+                    elif node.name == "sram_rtsel":
+                        node.name = self.sram_macro_info.rtsel_port_name
 
         v = SRAMPortNames(use_sram_stub, sram_macro_info)
         v.visit_root(generator)
