db6678890daddd875b53cebe16a318a69272ba79
Keyi Zhang <keyi@stanford.edu>
2020-04-01 14:43:42 -0700

    simplify the config extration pass

--- lake/top/extract_top_config.py
+++ lake/top/extract_top_config.py
@@ -14,15 +14,11 @@ if __name__ == "__main__":
     # Now get the config registers from the top definition
     for port_name in int_gen.get_port_names():
         curr_port = int_gen.get_port(port_name)
-        if(len(curr_port.get_attributes()) <= 0):
+        attrs = curr_port.find_attribute(lambda a: isinstance(a, ConfigRegAttr))
+        if len(attrs) != 1:
             continue
-        else:
-            cr_attr = None
-            for i in range(len(curr_port.get_attributes())):
-                if(isinstance(curr_port.get_attributes()[i].get(), ConfigRegAttr)):
-                    cr_attr = curr_port.get_attributes()[i].get()
-            if cr_attr:
-                for i in range(len(curr_port.size)):
-                    curr_port.size[i] -= 1
-                print(f"{curr_port.size}[{curr_port.width - 1}:0] {port_name}: " +
-                      f"{cr_attr.get_documentation()}")
+        cr_attr = attrs[0]
+        for i in range(len(curr_port.size)):
+            curr_port.size[i] -= 1
+        print(f"{curr_port.size}[{curr_port.width - 1}:0] {port_name}: " +
+              f"{cr_attr.get_documentation()}")
