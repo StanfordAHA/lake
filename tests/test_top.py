967de808d9dac6ebbc4d97e692c5a1ae398d7cc6
Kavya Sreedhar <skavya@kiwi.stanford.edu>
2020-04-09 12:21:35 -0700

    initial id stream test chaining

--- tests/test_top.py
+++ tests/test_top.py
@@ -7,7 +7,7 @@ import tempfile
 from lake.passes.passes import lift_config_reg, change_sram_port_names
 from lake.models.lake_top_model import LakeTopModel
 from utils.sram_macro import SRAMMacroInfo
-
+from lake.top.lake_chain import LakeChain
 
 
 @pytest.mark.parametrize("tb0_range_outer", [3, 4])
@@ -332,138 +332,65 @@ def test_sram_port_names_change(mem_width,
                                   additional_passes={"change_sram_port": change_sram_port_pass})
 
 
-def test_identity_stream(data_width=16,
-                         mem_width=64,
-                         mem_depth=16,
-                         banks=2,
-                         input_iterator_support=6,
-                         output_iterator_support=6,
-                         interconnect_input_ports=1,
-                         interconnect_output_ports=3,
-                         mem_input_ports=1,
-                         mem_output_ports=1,
-                         use_sram_stub=1,
-                         agg_height=8,
-                         transpose_height=8,
-                         max_agg_schedule=64,
-                         input_max_port_sched=64,
-                         output_max_port_sched=64,
-                         align_input=1,
-                         max_line_length=256,
-                         max_tb_height=1,
-                         tb_range_max=64,
-                         tb_sched_max=64,
-                         num_tb=1,
-                         tb_iterator_support=2,
-                         multiwrite=1,
-                         max_prefetch=64,
-                         num_tiles=2):
+def test_identity_stream(num_tiles=2, 
+                         interconnect_output_ports=1,
+                         interconnect_input_ports=3):
 
     new_config = {}
 
-    # Agg align
-    new_config["strg_ub_agg_align_0_line_length"] = 64
+    for i in range(num_tiles):
+        # Agg align
+        new_config[f"tile_{i}_strg_ub_agg_align_0_line_length"] = 64
 
-    # Agg buffer
-    new_config["strg_ub_agg_in_0_in_period"] = 1  # I don't actually know
-    new_config["strg_ub_agg_in_0_in_sched_0"] = 0
-    new_config["strg_ub_agg_in_0_out_period"] = 1
-    new_config["strg_ub_agg_in_0_out_sched_0"] = 0
+        # Agg buffer
+        new_config[f"tile_{i}_strg_ub_agg_in_0_in_period"] = 1  # I don't actually know
+        new_config[f"tile_{i}_strg_ub_agg_in_0_in_sched_0"] = 0
+        new_config[f"tile_{i}_strg_ub_agg_in_0_out_period"] = 1
+        new_config[f"tile_{i}_strg_ub_agg_in_0_out_sched_0"] = 0
 
-    # Input addr ctrl
-    new_config["strg_ub_input_addr_ctrl_address_gen_0_dimensionality"] = 2
-    new_config["strg_ub_input_addr_ctrl_address_gen_0_starting_addr"] = 0
-    new_config["strg_ub_input_addr_ctrl_address_gen_0_ranges_0"] = 12
-    new_config["strg_ub_input_addr_ctrl_address_gen_0_strides_0"] = 1
-    new_config["strg_ub_input_addr_ctrl_address_gen_0_ranges_1"] = 100
-    new_config["strg_ub_input_addr_ctrl_address_gen_0_strides_1"] = 12
-
-    # Output addr ctrl
-    new_config["strg_ub_output_addr_ctrl_address_gen_0_dimensionality"] = 1
-    new_config["strg_ub_output_addr_ctrl_address_gen_0_ranges_0"] = 3
-    new_config["strg_ub_output_addr_ctrl_address_gen_0_starting_addr"] = 0
-    new_config["strg_ub_output_addr_ctrl_address_gen_0_strides_0"] = 1
-
-    # TBA
-
-    # NOTE: both these configurations result in equivalent functionality
-
-    # if dimensionality == 1 version
-    new_config["strg_ub_tba_0_tb_0_range_outer"] = 12
-    new_config["strg_ub_tba_0_tb_0_stride"] = 1
-    new_config["strg_ub_tba_0_tb_0_dimensionality"] = 1
-
-    # if dimensionality == 2 version
-    new_config["strg_ub_tba_0_tb_0_indices_0"] = 0
-    new_config["strg_ub_tba_0_tb_0_indices_1"] = 1
-    new_config["strg_ub_tba_0_tb_0_indices_2"] = 2
-    new_config["strg_ub_tba_0_tb_0_indices_3"] = 3
-    new_config["strg_ub_tba_0_tb_0_range_inner"] = 4
-    # new_config["tba_0_tb_0_range_outer"] = 3
+        # Input addr ctrl
+        new_config[f"tile_{i}_strg_ub_input_addr_ctrl_address_gen_0_dimensionality"] = 2
+        new_config[f"tile_{i}_strg_ub_input_addr_ctrl_address_gen_0_starting_addr"] = 0
+        new_config[f"tile_{i}_strg_ub_input_addr_ctrl_address_gen_0_ranges_0"] = 12
+        new_config[f"tile_{i}_strg_ub_input_addr_ctrl_address_gen_0_strides_0"] = 1
+        new_config[f"tile_{i}_strg_ub_input_addr_ctrl_address_gen_0_ranges_1"] = 100
+        new_config[f"tile_{i}_strg_ub_input_addr_ctrl_address_gen_0_strides_1"] = 12
+    
+        # Output addr ctrl
+        new_config[f"tile_{i}_strg_ub_output_addr_ctrl_address_gen_0_dimensionality"] = 1
+        new_config[f"tile_{i}_strg_ub_output_addr_ctrl_address_gen_0_ranges_0"] = 3
+        new_config[f"tile_{i}_strg_ub_output_addr_ctrl_address_gen_0_starting_addr"] = 0
+        new_config[f"tile_{i}_strg_ub_output_addr_ctrl_address_gen_0_strides_0"] = 1
+    
+        # TBA
+    
+        # NOTE: both these configurations result in equivalent functionality
+    
+        # if dimensionality == 1 version
+        new_config[f"tile_{i}_strg_ub_tba_0_tb_0_range_outer"] = 12
+        new_config[f"tile_{i}_strg_ub_tba_0_tb_0_stride"] = 1
+        new_config[f"tile_{i}_strg_ub_tba_0_tb_0_dimensionality"] = 1
+    
+        # if dimensionality == 2 version
+        new_config[f"tile_{i}_strg_ub_tba_0_tb_0_indices_0"] = 0
+        new_config[f"tile_{i}_strg_ub_tba_0_tb_0_indices_1"] = 1
+        new_config[f"tile_{i}_strg_ub_tba_0_tb_0_indices_2"] = 2
+        new_config[f"tile_{i}_strg_ub_tba_0_tb_0_indices_3"] = 3
+        new_config[f"tile_{i}_strg_ub_tba_0_tb_0_range_inner"] = 4
+        # new_config["tba_0_tb_0_range_outer"] = 3
     # new_config["tba_0_tb_0_stride"] = 4
     # new_config["tba_0_tb_0_tb_height"] = 1
     # new_config["tba_0_tb_0_dimensionality"] = 2
 
     # Sets multiwrite
-    new_config["strg_ub_input_addr_ctrl_offsets_cfg_0_0"] = 0
+        new_config[f"tile_{i}_strg_ub_input_addr_ctrl_offsets_cfg_0_0"] = 0
 
-    new_config["strg_ub_sync_grp_sync_group_0"] = 1
+        new_config[f"tile_{i}_strg_ub_sync_grp_sync_group_0"] = 1
 
-    new_config["chain_idx"] = 0
-    
-    model_lt = LakeTopModel(data_width=data_width,
-                            mem_width=mem_width,
-                            mem_depth=mem_depth,
-                            banks=banks,
-                            input_iterator_support=input_iterator_support,
-                            output_iterator_support=output_iterator_support,
-                            interconnect_input_ports=interconnect_input_ports,
-                            interconnect_output_ports=interconnect_output_ports,
-                            mem_input_ports=mem_input_ports,
-                            mem_output_ports=mem_output_ports,
-                            use_sram_stub=use_sram_stub,
-                            agg_height=agg_height,
-                            max_agg_schedule=max_agg_schedule,
-                            input_max_port_sched=input_max_port_sched,
-                            output_max_port_sched=output_max_port_sched,
-                            align_input=align_input,
-                            max_line_length=max_line_length,
-                            tb_height=max_tb_height,
-                            tb_range_max=tb_range_max,
-                            tb_sched_max=tb_sched_max,
-                            num_tb=num_tb,
-                            multiwrite=multiwrite,
-                            max_prefetch=max_prefetch,
-                            num_tiles=num_tiles)
-
-    model_lt.set_config(new_config=new_config)
+        new_config[f"tile_{i}_strg_ub_tba_0_tb_0_tb_height"] = 1
 
     ### DUT
-    lt_dut = LakeTop(data_width=data_width,
-                     mem_width=mem_width,
-                     mem_depth=mem_depth,
-                     banks=banks,
-                     input_iterator_support=input_iterator_support,
-                     output_iterator_support=output_iterator_support,
-                     interconnect_input_ports=interconnect_input_ports,
-                     interconnect_output_ports=interconnect_output_ports,
-                     mem_input_ports=mem_input_ports,
-                     mem_output_ports=mem_output_ports,
-                     use_sram_stub=use_sram_stub,
-                     agg_height=agg_height,
-                     max_agg_schedule=max_agg_schedule,
-                     input_max_port_sched=input_max_port_sched,
-                     output_max_port_sched=output_max_port_sched,
-                     align_input=align_input,
-                     max_line_length=max_line_length,
-                     max_tb_height=max_tb_height,
-                     tb_range_max=tb_range_max,
-                     tb_sched_max=tb_sched_max,
-                     num_tb=num_tb,
-                     tb_iterator_support=tb_iterator_support,
-                     multiwrite=multiwrite,
-                     max_prefetch=max_prefetch,
-                     num_tiles=num_tiles)
+    lt_dut = LakeChain(num_tiles=num_tiles)
 
     # Run the config reg lift
     lift_config_reg(lt_dut.internal_generator)
@@ -478,13 +405,6 @@ def test_identity_stream(data_width=16,
     ###
     for key, value in new_config.items():
         setattr(tester.circuit, key, value)
-
-    tester.circuit.strg_ub_tba_0_tb_0_tb_height = 1
-
-    if interconnect_output_ports == 1:
-        tester.circuit.strg_ub_sync_grp_sync_group[0] = 1
-
-    # tester.circuit.chain_idx = 1
 
     rand.seed(0)
     tester.circuit.clk = 0
@@ -509,15 +429,12 @@ def test_identity_stream(data_width=16,
         output_en = rand.randint(0, 1)
         if(interconnect_input_ports == 1):
             tester.circuit.data_in = data_in[0]
-            tester.chain_output_in = data_in[0]
             tester.circuit.wen = valid_in[0]
         else:
             for j in range(interconnect_input_ports):
                 setattr(tester.circuit, f"data_in_{j}", data_in[j])
-                setattr(tester.circuit, f"chain_output_in_{j}", data_in[j])
-                tester.circuit.wen[j] = valid_in[j]
+                setattr(tester.circuit, f"wen_{j}", valid_in[j])
 
-            # setattr(tester.circuit, f"wen_{j}", valid_in[j])
         tester.circuit.addr_in = addr_in
         tester.circuit.wen_en = wen_en
 
@@ -525,36 +442,16 @@ def test_identity_stream(data_width=16,
         enable_chain_output = 0
         tester.enable_chain_output = enable_chain_output
 
-        if(interconnect_output_ports == 1):
-            chain_valid_in = 1
-            tester.circuit.chain_valid_in = chain_valid_in
-        else:
-            chain_valid_in = []
-            for j in range(interconnect_output_ports):
-                setattr(tester.circuit, f"chain_valid_in_{j}", 1)
-                chain_valid_in.append(1)
-
         for j in range(interconnect_output_ports):
             tester.circuit.ren_en[j] = ren_en[j]
 
-        (mod_do, mod_vo, chain_data, valid_data) = model_lt.interact(data_in, addr_in, valid_in, wen_en, ren_en, output_en, enable_chain_output, data_in, chain_valid_in)
         tester.circuit.output_en = output_en
 
         if i > 200:
             for j in range(interconnect_output_ports):
                 ren_en[j] = 1
+        
         tester.eval()
-
-        # Now check the outputs
-        if(interconnect_output_ports == 1):
-            tester.circuit.valid_out.expect(mod_vo[0])
-            if mod_vo[0]:
-                tester.circuit.data_out.expect(mod_do[0][0])
-        else:
-            for j in range(interconnect_output_ports):
-                tester.circuit.valid_out[j].expect(mod_vo[j])
-                if mod_vo[j]:
-                    getattr(tester.circuit, f"data_out_{j}").expect(mod_do[j][0])
 
         tester.step(2)
 
