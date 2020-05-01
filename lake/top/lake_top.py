33d2c20d33cc618ca7159012fc6cb62d3a5cd7e7
mbstrange2 <mstrange@stanford.edu>
2020-05-01 10:20:25 -0700

    casting doesn't work either

--- lake/top/lake_top.py
+++ lake/top/lake_top.py
@@ -222,7 +222,7 @@ class LakeTop(Generator):
 
         # Currenlt mode = 0 is UB, mode = 1 is FIFO
         self._gclk = self.var("gclk", 1)
-        self.wire(self._gclk, self._clk & self._tile_en)
+        self.wire(self._gclk, kts.util.clock(self._clk & self._tile_en))
 
         self._mem_data_out = self.var("mem_data_out",
                                       self.data_width,
