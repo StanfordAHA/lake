926fae61ef0c5e1f49674526b317892b7dd199b3
Kavya Sreedhar <skavya@kiwi.stanford.edu>
2020-06-29 21:49:58 -0700

    deleting a lot of tba, just creating tb

--- lake/modules/transpose_buffer_aggregation.py
+++ lake/modules/transpose_buffer_aggregation.py
@@ -65,24 +65,6 @@ class TransposeBufferAggregation(Generator):
         self.tb_to_interconnect_valid = self.output("tb_to_interconnect_valid", 1)
         self.tb_arbiter_rdy = self.output("tb_arbiter_rdy", 1)
 
-        # local variables
-        self.valid_data_all = self.var("valid_data_all",
-                                       width=1,
-                                       size=self.num_tb,
-                                       packed=True)
-
-        self.tb_output_data_all = self.var("tb_output_data_all",
-                                           width=self.word_width,
-                                           size=[self.num_tb, self.max_tb_height],
-                                           packed=True)
-        self.tb_output_valid_all = self.var("tb_output_valid_all", width=1, size=self.num_tb, packed=True)
-        self.tb_arbiter_rdy_all = self.var("tb_arbiter_rdy_all", width=1, size=self.num_tb, packed=True)
-        self.output_valid = self.var("output_valid", 1)
-        self.output_inter = self.var("output_inter",
-                                     width=self.word_width,
-                                     size=self.max_tb_height,
-                                     packed=True)
-
         for i in range(self.num_tb):
             self.add_child(f"tb_{i}",
                            TransposeBuffer(self.word_width,
@@ -96,54 +78,13 @@ class TransposeBufferAggregation(Generator):
                            clk=self.clk,
                            rst_n=self.rst_n,
                            input_data=self.SRAM_to_tb_data,
-                           valid_data=self.valid_data_all[i],
-                           col_pixels=self.tb_output_data_all[i],
-                           output_valid=self.tb_output_valid_all[i],
-                           rdy_to_arbiter=self.tb_arbiter_rdy_all[i],
+                           valid_data=self.valid_data,
+                           col_pixels=self.tb_to_interconnect_data,
+                           output_valid=self.tb_to_interconnect_valid,
+                           rdy_to_arbiter=self.tb_arbiter_rdy,
                            ack_in=self._ack_in,
                            mem_valid_data=self.mem_valid_data,
                            ren=self.tba_ren)
-
-        self.add_code(self.set_valid_data_all)
-        self.set_output_valid()
-        self.send_tba_rdy()
-
-    @always_comb
-    def set_valid_data_all(self):
-        if ~self.valid_data:
-            for i in range(self.num_tb):
-                self.valid_data_all[i] = 0
-        else:
-            for i in range(self.num_tb):
-                if i == self.tb_index_for_data:
-                    self.valid_data_all[i] = 1
-                else:
-                    self.valid_data_all[i] = 0
-
-    def set_output_valid(self):
-        comb_output = self.combinational()
-        valid_count = self.tb_output_valid_all[0]
-        comb_output.add_stmt(self.output_inter.assign(self.tb_output_data_all[0]))
-        for i in range(1, self.num_tb):
-            valid_count = valid_count + self.tb_output_valid_all[i]
-            if_vld = IfStmt(self.tb_output_valid_all[i] == 1)
-            if_vld.then_(self.output_inter.assign(self.tb_output_data_all[i]))
-            comb_output.add_stmt(if_vld)
-        if_vld_count = IfStmt(valid_count > 0)
-        if_vld_count.then_(self.tb_to_interconnect_valid.assign(1))
-        if_vld_count.else_(self.tb_to_interconnect_valid.assign(0))
-        comb_output.add_stmt(self.tb_to_interconnect_data.assign(self.output_inter))
-        comb_output.add_stmt(if_vld_count)
-
-    def send_tba_rdy(self):
-        comb_tb_arbiter_rdy = self.combinational()
-        rdy_count = self.tb_arbiter_rdy_all[0]
-        for i in range(1, self.num_tb):
-            rdy_count = rdy_count + self.tb_arbiter_rdy_all[i]
-        if_rdy_count = IfStmt(rdy_count > 0)
-        if_rdy_count.then_(self.tb_arbiter_rdy.assign(1))
-        if_rdy_count.else_(self.tb_arbiter_rdy.assign(0))
-        comb_tb_arbiter_rdy.add_stmt(if_rdy_count)
 
 
 if __name__ == "__main__":
