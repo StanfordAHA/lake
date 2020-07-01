97dec8437afba7036b457e870087a356c169239d
Kavya Sreedhar <skavya@kiwi.stanford.edu>
2020-07-01 10:59:10 -0700

    some prints and python model working

--- lake/modules/hwtypes/sram_stub.py
+++ lake/modules/hwtypes/sram_stub.py
@@ -6,15 +6,24 @@ import hwtypes
 import fault
 
 
-def get_slice(data, addr, width):
+def get_slice(data, addr_, width):
+    addr = addr_.zext(data.size - addr_.size)
     return (data >> addr)[:width]
 
 def set_slice(data, addr_, width, value):
     assert value.size == width
+    #print("set_slice func")
+    #print("data ", data)
+    #print("addr ", addr_)
+    #print("width ", width)
+    #print("value ", value)
     addr = addr_.zext(data.size - addr_.size)
+    #print("addr ", addr)
     top_bits = (data >> (addr + width)) << (addr + width)
     mid_bits = value.zext(data.size - width) << addr
+    #print("mid bits ", mid_bits)
     bot_bits = (data << (addr + width)) >> (addr + width)
+    #print(top_bits | mid_bits | bot_bits)
     return top_bits | mid_bits | bot_bits
 
 def sram_stub(
@@ -38,21 +47,29 @@ def sram_stub(
 
             @name_outputs(data_out=WideData)
             def __call__(self,
-                    wen: Bit = Bit(0), \
                     cen: Bit = Bit(0), \
+                    wen: Bit = Bit(0), \
                     addr: BitVector[addr_width] = BitVector[addr_width](0), \
                     data_in: WideData = WideData(0)
                     ) -> (WideData):
 
-                if cen == Bit(1) and wen == Bit(1):
-                    set_slice(self.mem, addr, data_width, data_in)
+                print("wen: ", wen, " cen: ", cen, " data_in ", data_in, " addr: ", addr)
+                print("mem: ", self.mem)
+                if (cen == Bit(1)) and (wen == Bit(1)):
+                    print("setting slice")
+                    self.mem = set_slice(self.mem, addr, data_width, data_in)
+                    # print("mem after set slice ", self.mem)
 
-                if cen == Bit(1) and wen == Bit(0):
+                if (cen == Bit(1)) and (wen == Bit(0)):
+                    print("getting slice")
+                    print("get slice mem ", self.mem)
                     # data_out = self.mem[addr * data_width : (addr + 1) * data_width]
                     data_out = get_slice(self.mem, addr * data_width, data_width)
                 else:
                     data_out = WideData(0)
 
+                print("mem: ", self.mem)
+                print("data out: ", data_out)
                 return data_out
 
         return SRAMStub
@@ -60,22 +77,32 @@ def sram_stub(
     return modules_fc
 
 if __name__ == "__main__":
-    mem_depth = 4
-    data_width = 64
-    sram_py = sram_stub(mem_depth, data_width, family.PyFamily())
-    sram_py(family=family.PyFamily())()(hwtypes.Bit(1), hwtypes.Bit(1), hwtypes.BitVector[log2(mem_depth)](2), hwtypes.BitVector[data_width](2))
-
-    sram_magma = sram_stub(family=family.MagmaFamily())
-    tester = fault.Tester(sram_magma(family=family.MagmaFamily())())
-    data = 0
-    for i in range(5):
-        tester.circuit.wen = i % 2
-        tester.circuit.cen = 1
-        tester.circuit.data_in = data
-        tester.circuit.addr = data
-        data = data + 1
-        tester.eval()
-        tester.step(2)
-
-    tester.compile_and_run("verilator", flags=["-Wno-fatal"])
+    pyt = True
+    if pyt:
+        mem_depth = 4
+        data_width = 64
+        sram_py = sram_stub(mem_depth, data_width, family.PyFamily())
+        sram_py_inst = sram_py(family=family.PyFamily())()
+        a = sram_py_inst(hwtypes.Bit(1), hwtypes.Bit(1), hwtypes.BitVector[log2(mem_depth)](0), hwtypes.BitVector[data_width](1))
+        print(a)
+        b = sram_py_inst(hwtypes.Bit(1), hwtypes.Bit(0), hwtypes.BitVector[log2(mem_depth)](0), hwtypes.BitVector[data_width](1))
+        print(b)
+        c = sram_py_inst(hwtypes.Bit(1), hwtypes.Bit(1), hwtypes.BitVector[log2(mem_depth)](1), hwtypes.BitVector[data_width](2))
+        print(c)
+        d = sram_py_inst(hwtypes.Bit(1), hwtypes.Bit(0), hwtypes.BitVector[log2(mem_depth)](1), hwtypes.BitVector[data_width](2))
+        print(d)
+    else:
+        sram_magma = sram_stub(family=family.MagmaFamily())
+        tester = fault.Tester(sram_magma(family=family.MagmaFamily())())
+        data = 0
+        for i in range(5):
+            tester.circuit.wen = i % 2
+            tester.circuit.cen = 1
+            tester.circuit.data_in = data
+            tester.circuit.addr = data
+            data = data + 1
+            tester.eval()
+            tester.step(2)
+    
+        tester.compile_and_run("verilator", flags=["-Wno-fatal"])
 
