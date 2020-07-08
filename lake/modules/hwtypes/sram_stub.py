88fcb7885d6d27c06ac65c067901595d4c0db53c
Kavya Sreedhar <skavya@kiwi.stanford.edu>
2020-07-07 21:54:52 -0700

    adding Signed instead for magma error

--- lake/modules/hwtypes/sram_stub.py
+++ lake/modules/hwtypes/sram_stub.py
@@ -22,9 +22,9 @@ def sram_stub(
 
     @family_closure
     def modules_fc(family):
-        BitVector = family.Unsigned
+        BitVector = family.Signed
         Bit = family.Bit
-        WideData = family.Unsigned[data_width]
+        WideData = family.Signed[data_width]
 
         @family.assemble(locals(), globals())
         class SRAMStub():
@@ -42,8 +42,7 @@ def sram_stub(
                 # print("wen: ", wen, " cen: ", cen, " data_in ", data_in, " addr: ", addr)
                 # print("mem: ", self.mem)
                 if cen & wen:
-                    # print("setting slice")
-                    print("set slice addr ", addr)
+                    # print("set slice addr ", addr)
                     result = set_slice(self.mem, addr, data_width, data_in)
                     self.mem = result
                     # print("mem after set slice ", self.mem)
@@ -55,7 +54,6 @@ def sram_stub(
                 else:
                     data_out = WideData(0)
                 
-                # print("mem: ", self.mem)
                 # print("mem: ", self.mem)
                 # print("data out: ", data_out)
                 return data_out
@@ -80,23 +78,22 @@ if __name__ == "__main__":
 
     model_sram = SRAMModel(data_width, fetch_width, mem_depth, 1)
 
+    rand.seed(0)
+
     x = 0
     for i in range(16):
-        wen = 1 - (i % 2) #rand.randint(0, 1)
+        wen = 1 - (i % 2) # rand.randint(0, 1)
         cen = 1 # rand.randint(0, 1)
-        addr = x #rand.randint(0, mem_depth - 1)
-        data = x + 1#rand.randint(0, 2**(data_width * fetch_width) - 1)
+        addr = x # rand.randint(0, mem_depth - 1)
+        data = x + 1 # rand.randint(0, 2**(data_width * fetch_width) - 1)
     
-        py_data_out = sram_py_inst(py_fam.Bit(wen), py_fam.Bit(cen), py_fam.BitVector[log2(mem_depth)](addr), py_fam.BitVector[data_width * fetch_width](data))
-        print(f'wen: {wen}, cen: {cen}, addr: {addr}, data: {data}')
-        model_data_out = model_sram.interact(wen, cen, addr, [0, 0, 0, data])
-   #     if cen and not wen:
-        print(f'model {model_data_out} py_dat {py_data_out}')
-    #        assert model_data_out[0] == py_data_out
-        if i % 2:
-            x = x + 1
+        print(f'wen: {wen}, cen: {cen}, addr: {addr}, data: {data}') 
 
-        '''if i % 2:
+        py_data_out = sram_py_inst(py_fam.Bit(wen), py_fam.Bit(cen), py_fam.BitVector[log2(mem_depth)](addr), py_fam.BitVector[data_width * fetch_width](data))
+
+        model_data_out = model_sram.interact(wen, cen, addr, [0, 0, 0, data])
+        
+        if i % 2:
             x = x + 1
         tester.circuit.wen = wen
         tester.circuit.cen = cen
@@ -105,32 +102,13 @@ if __name__ == "__main__":
         tester.eval()
         if cen and (not wen):
             tester.circuit.O.expect(py_data_out)
-        print(model_data_out)
-        print(py_data_out)
+            print(model_data_out)
+            print(py_data_out)
         # assert model_data_out[0] == py_data_out
-        # print(py_data_out)
         tester.step(2)
 
         with tempfile.TemporaryDirectory() as tempdir:
             tempdir="output_m"
             tester.compile_and_run(target="verilator",
                                    directory=tempdir,
-                                   flags=["-Wno-fatal", "--trace"])'''
-
-        # Caleb test
-        '''    # functional model
-    sram_py = sram_stub()
-    py_fam = family.PyFamily()
-    py_sram = sram_py(family=py_fam)()
-    mem_depth = 4
-    data_width = 64
-    num_bits = mem_depth * data_width
-    addr_width = int(log2(mem_depth))                                                                                                                                                        
-    for wen in (1, 0):
-        wen = py_fam.Bit(wen)
-        cen = py_fam.Bit(1)
-        for addr in range(4):
-            addr = py_fam.BitVector[addr_width](addr)
-            data = py_fam.BitVector[data_width](rand.randrange(0, 1 << data_width))
-            print(f'wen: {wen}, cen: {cen}, addr: {addr}, data: {data}')
-            print(py_sram(wen, cen, addr, data))'''
+                                   flags=["-Wno-fatal", "--trace"])
