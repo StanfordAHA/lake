493864a72b340fa69256e91ade61a15983cfe900
Kavya Sreedhar <skavya@kiwi.stanford.edu>
2020-09-16 10:57:53 -0700

    checkpoint, not yet tested mem

--- collateral2compiler/memory.py
+++ collateral2compiler/memory.py
@@ -1,5 +1,6 @@
 from kratos import *
 from math import log
+from collateral2compiler import port
 
 
 class Memory(Generator):
@@ -44,10 +45,6 @@ class Memory(Generator):
 
         if read_write_info is not None:
             self.read_write_addr = self.input("read_write_addr", clog2(mem_width))
-        if read_info is not None:
-            self.read_addr = self.input("read_addr", clog2(mem_width))
-        if write_info is not None:
-            self.write_addr = self.input("write_addr", clog2(mem_width))
 
         self.data_out = self.output("data_out",
                                     width=word_width,
@@ -64,11 +61,36 @@ class Memory(Generator):
         if read_write_info is not None:
             if read_write_info["latency"] == 1:
 
+        split_addr_signals = False
         if write_info is not None:
+            self.write_addr = self.input("write_addr", clog2(mem_width))
+            if write_info["latency"] == 1:
+                self.write_addr_bank = self.var("write_addr_bank",
+                                                self.write_addr.width - clog2(write_port_width))
+                self.write_addr_port = self.var("write_addr_port", clog2(write_port_width))
+                self.add_code(write_data_latency_1)
+                split_addr_signals = True
+            else:
+                self.add_code(write_data_latency_0)
+
+        if read_info is not None:
+            self.read_addr = self.input("read_addr", clog2(mem_width)) 
+            if read_info["latency"] == 1:
+                self.add_code(read_data_latency_1)
+            else:
+                self.read_addr_bank = self.var("read_addr_bank",
+                                               clog2(num_banks) + clog2(read_port_width) - clog2(read_port_width))
+                self.read_addr_port = self.var("read_addr_port", clog2(read_port_width))
+                self.add_code(read_data_latency_0)
+                split_addr_signals = True
+
+        if split_addr_signals:
+            self.add_code(self.split_addr)
+
         self.add_code(self.output_data)
 
     @always_ff((posedge, "clk"), (negedge, "rst_n"))
-    def write_data_latency_1(self, idx):
+    def write_data_latency_1(self):
         if ~rst_n:
             for bank in range(num_banks):
                 for port in range(mem_width):
@@ -84,23 +106,36 @@ class Memory(Generator):
                 self.memory[bank][port] = self.data_in[bank][port]
 
     
-    @always_ff((posedge, "clk"), (negedge, "rst_n"))
-    def read_data_latency_1(self):
-        self.memory
+    #@always_ff((posedge, "clk"), (negedge, "rst_n"))
+    #def read_data_latency_1(self):
+    #    self.memory
 
     @always_comb
     def read_data_latency_0(self):
         for bank in range(num_banks):
             for port in range(read_port_width):
-                self.data_out[bank][port] = self.memory[self.read_addr][port]
+                self.data_out[bank][port] = self.memory[self.read_addr_bank][self.read_addr_port]
 
     @always_comb
     def split_addr(self):
         self.write_addr_bank = self.write_addr[self.write_addr.width - 1, clog2(write_port_width)]
         self.write_addr_port = self.write_addr[clog2(write_port_width) - 1, 0]
+        self.read_addr_bank = self.read_addr[clog2(num_banks) + clog2(read_port_width) - 1, clog2(read_port_width)]
+        self.read_addr_port = self.read_addr[clog2(read_port_width) - 1, 0]
 
 
 if __name__ == "__main__":
-    db_dut = Aggregator(word_width=16,
-                        mem_word_width=4)
-    verilog(db_dut, filename="aggregator.sv")
+    Memory(
+           capacity,
+           word_width,
+           num_read_ports,
+           read_port_width,
+           num_write_ports,
+           write_port_width,
+           num_read_write_ports,
+           read_write_port_width,
+           num_banks,
+           chaining,
+           read_write_info,
+           write_info,
+           read_info):
---
+++ collateral2compiler/port.py
@@ -0,0 +1,9 @@
+class Port():
+    def __init__(self,
+                 latency,
+                 consecutive_latency,
+                 gen_dim=6,
+                 gen_range=65536,
+                 gen_stride=65536):
+
+        self.port_info = {"latency": latency, "consecutive_latency": consecutive_latency}
