ac06715c5cf53c86360401be4eddc2263e9eca51
mbstrange2 <mstrange@stanford.edu>
2020-09-17 12:00:02 -0700

    force errors with -e

--- .travis/run.sh
+++ .travis/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-
 set +x
+set -e
 export LAKE_STREAM="$(pip show lake | grep 'Location' | cut -d' ' -f2)/sequences/"
 cd / && git clone https://github.com/dillonhuff/clockwork && cd clockwork && git checkout lower_ubuffer
 export LAKE_CONTROLLERS="/clockwork/lake_controllers/"
@@ -10,6 +10,6 @@ export PYTEST_ADDOPTS="--color=yes"
 cd lake
 python3 -m pycodestyle lake/
 python3 -m pycodestyle tests/
-pytest -vs tests/test_lake.py
+pytest -vs tests/
 
 set -x
