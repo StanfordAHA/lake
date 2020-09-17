51d8f119de1206011f1ce67b84cb08bd9de48921
mbstrange2 <mstrange@stanford.edu>
2020-09-17 11:17:18 -0700

    update travis run - got permission denied?

--- .travis/run.sh
+++ .travis/run.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 set +x
 export LAKE_STREAM="$(pip show lake | grep 'Location' | cut -d' ' -f2)/sequences/"
 cd / && git clone https://github.com/dillonhuff/clockwork && cd clockwork && git checkout lower_ubuffer
