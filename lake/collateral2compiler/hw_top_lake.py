1fe44e0c0117369e9a566583c005d4e9950bb17e
Kavya Sreedhar <skavya@stanford.edu>
2020-10-29 12:52:59 -0700

    initial static bitstream commit

--- lake/collateral2compiler/hw_top_lake.py
+++ lake/collateral2compiler/hw_top_lake.py
@@ -1,13 +1,17 @@
 from kratos import *
+import kratos as kts
+from _kratos import create_wrapper_flatten
 from math import log
+
+from lake.attributes.config_reg_attr import ConfigRegAttr
 from lake.collateral2compiler.memory import mem_inst
 from lake.collateral2compiler.helper import *
 from lake.modules.for_loop import ForLoop
 from lake.modules.addr_gen import AddrGen
 from lake.modules.spec.sched_gen import SchedGen
 from lake.passes.passes import lift_config_reg
-from lake.utils.util import safe_wire
-
+from lake.utils.util import safe_wire, trim_config_list
+from lake.utils.parse_clkwork_config import * 
 
 # TO DO if write, need to check if that happens for read/write ports
 class TopLakeHW(Generator):
@@ -25,6 +29,8 @@ class TopLakeHW(Generator):
         self.input_ports = input_ports
         self.output_ports = output_ports
 
+        self.stencil_valid = False
+
         # objects
         self.memories = memories
         self.edges = edges
@@ -32,6 +38,9 @@ class TopLakeHW(Generator):
         self.default_config_width = 16
 
         # inputs
+        self.tile_en = self.input("tile_en", 1)
+        self.tile_en.add_attribute(ConfigRegAttr("Tile logic enable manifested as clock gate"))
+
         self.clk = self.clock("clk")
         # active low asynchornous reset
         self.rst_n = self.reset("rst_n", 1)
@@ -101,27 +110,26 @@ class TopLakeHW(Generator):
         for i in range(len(is_input)):
             in_mem = is_input[i]
 
-            # with static schedule, incoming data is always written
-            self.wire(self.mem_insts[in_mem].ports.write, self.high)
+            self.valid = self.var(f"input2{in_mem}_accessor_valid", 1)
+            self.wire(self.mem_insts[in_mem].ports.write, self.valid)
 
             safe_wire(self, self.mem_insts[in_mem].ports.data_in[0], self.data_in[i])
 
-            self.valid = self.var(in_mem + "_accessor_valid", 1)
 
             forloop = ForLoop(iterator_support=6,
-                              config_width=self.default_config_width)
+                              config_width=16)#self.default_config_width)
             loop_itr = forloop.get_iter()
             loop_wth = forloop.get_cfg_width()
 
-            self.add_child(in_mem + "_forloop",
+            self.add_child(f"input2{in_mem}_forloop",
                            forloop,
                            clk=self.clk,
                            rst_n=self.rst_n,
                            step=self.valid)
 
             newAG = AddrGen(iterator_support=6,
-                            config_width=self.default_config_width)
-            self.add_child(in_mem + "write_addr_gen",
+                            config_width=4)#self.default_config_width)
+            self.add_child(f"input2{in_mem}_write_addr_gen",
                            newAG,
                            clk=self.clk,
                            rst_n=self.rst_n,
@@ -136,7 +144,7 @@ class TopLakeHW(Generator):
 
             newSG = SchedGen(iterator_support=6,
                              config_width=self.default_config_width)
-            self.add_child(in_mem + "write_sched_gen",
+            self.add_child(f"input2{in_mem}_write_sched_gen",
                            newSG,
                            clk=self.clk,
                            rst_n=self.rst_n,
@@ -150,14 +158,14 @@ class TopLakeHW(Generator):
             out_mem = is_output[i]
             self.wire(self.data_out[i], self.mem_data_outs[subscript_mems.index(out_mem)][0])  # , self.mem_insts[out_mem].ports.data_out)
 
-            self.valid = self.var(out_mem + "_accessor_valid", 1)
+            self.valid = self.var(f"{out_mem}2output_accessor_valid", 1)
 
             forloop = ForLoop(iterator_support=6,
                               config_width=self.default_config_width)
             loop_itr = forloop.get_iter()
             loop_wth = forloop.get_cfg_width()
 
-            self.add_child(out_mem + "_forloop",
+            self.add_child(f"{out_mem}2output_forloop",
                            forloop,
                            clk=self.clk,
                            rst_n=self.rst_n,
@@ -165,7 +173,7 @@ class TopLakeHW(Generator):
 
             newAG = AddrGen(iterator_support=6,
                             config_width=self.default_config_width)
-            self.add_child(out_mem + "write_addr_gen",
+            self.add_child(f"{out_mem}2output_read_addr_gen",
                            newAG,
                            clk=self.clk,
                            rst_n=self.rst_n,
@@ -180,7 +188,7 @@ class TopLakeHW(Generator):
 
             newSG = SchedGen(iterator_support=6,
                              config_width=self.default_config_width)
-            self.add_child(out_mem + "read_sched_gen",
+            self.add_child(f"{out_mem}2output_read_sched_gen",
                            newSG,
                            clk=self.clk,
                            rst_n=self.rst_n,
@@ -233,7 +241,11 @@ class TopLakeHW(Generator):
             if self.memories[edge["from_signal"][0]]["num_read_write_ports"] == 0:
                 # can assign same read addrs to all the memories
                 for i in range(len(edge["from_signal"])):
-                    safe_wire(self, self.mem_insts[edge["from_signal"][i]].ports.read_addr[0], readAG.ports.addr_out)
+                    # TO DO this should be deleted once mem addressing changes
+                    if edge["from_signal"] in ("agg", "agg1"):
+                        safe_wire(self, self.mem_insts[edge["from_signal"][i]].ports.read_addr[0], readAG.ports.addr_out*4)
+                    else:
+                        safe_wire(self, self.mem_insts[edge["from_signal"][i]].ports.read_addr[0], readAG.ports.addr_out)
             else:
                 # there needs to be an if valid check here
                 for i in range(len(edge["from_signal"])):
@@ -284,7 +296,11 @@ class TopLakeHW(Generator):
             # set write addr for to memories
             if self.memories[edge["to_signal"][0]]["num_read_write_ports"] == 0:
                 for i in range(len(edge["to_signal"])):
-                    safe_wire(self, self.mem_insts[edge["to_signal"][i]].ports.write_addr[0], writeAG.ports.addr_out)
+                    # TO DO this should be deleted once mem addressing changes
+                    if edge["to_signal"] in ("tb", "tb1"):
+                        safe_wire(self, self.mem_insts[edge["to_signal"][i]].ports.write_addr[0], writeAG.ports.addr_out*4)
+                    else:
+                        safe_wire(self, self.mem_insts[edge["to_signal"][i]].ports.write_addr[0], writeAG.ports.addr_out)
             else:
                 for i in range(len(edge["to_signal"])):
                     safe_wire(self, self.mem_insts[edge["to_signal"][i]].ports.read_write_addr[0], writeAG.ports.addr_out)
@@ -364,6 +380,113 @@ class TopLakeHW(Generator):
                 self.delayed_writes[i + 1] = self.delayed_writes[i]
             self.delayed_writes[0] = self.valid
 
-# the best way to unit test this file is to call construct_lake() in dsl.py
-# that function calls functions that prepare the user input data for this
-# file to generate the hardware
+    def get_static_bitstream(self,
+                             config_path,
+                             in_file_name,
+                             out_file_name):
+
+        input_ports = 1
+        output_ports = 1
+
+        in2agg = map_controller(extract_controller(config_path + '/' + in_file_name + '_in2agg_0.csv'), "in2agg")
+        agg2sram = map_controller(extract_controller(config_path + '/' + in_file_name + '_agg2sram.csv'), "agg2sram")
+        sram2tb = map_controller(extract_controller(config_path + '/' + out_file_name + '_2_sram2tb.csv'), "sram2tb")
+        tb2out0 = map_controller(extract_controller(config_path + '/' + out_file_name + '_2_tb2out_0.csv'), "tb2out0")
+        tb2out1 = map_controller(extract_controller(config_path + '/' + out_file_name + '_2_tb2out_1.csv'), "tb2out1")
+
+        # Getting bitstreams is a little unweildy due to fault (or its underlying implementation) not
+        # handling arrays in the interface.
+        # To alleviate this, we create the flattened wrapper so we can query widths of config
+        # registers and trim values to their bitwidths...
+        print(f"Current_name: {self.name}")
+        flattened = create_wrapper_flatten(self.internal_generator.clone(),
+                                           self.name + "_W")
+
+        # Set configuration...
+        config = [
+            ("agg_agg1_sram_edge_read_addr_gen_starting_addr", agg2sram.out_data_strt),
+            ("agg_agg1_sram_edge_write_addr_gen_starting_addr", agg2sram.in_data_strt),
+            ("agg_agg1_sram_edge_sched_gen_sched_addr_gen_starting_addr", agg2sram.cyc_strt),
+            ("agg_agg1_sram_edge_forloop_dimensionality", agg2sram.dim),
+
+            ("strg_sram_tb_tb1_edge_read_addr_gen_starting_addr", sram2tb.out_data_strt),
+            ("strg_ub_tb_write_addr_gen_0_starting_addr", sram2tb.in_data_strt),
+            ("strg_ub_tb_write_addr_gen_1_starting_addr", sram2tb.in_data_strt),
+            ("strg_ub_out_port_sel_addr_starting_addr", sram2tb.mux_data_strt),
+            ("sram_tb_tb1_edge_sched_gen_sched_addr_gen_starting_addr", sram2tb.cyc_strt),
+            ("sram_tb_tb1_edge_forloop_dimensionality", sram2tb.dim),
+
+            ("input2agg_write_addr_gen_starting_addr", in2agg.in_data_strt),
+            ("input2agg_write_sched_gen_sched_addr_gen_starting_addr", in2agg.cyc_strt),
+            ("input2agg_forloop_dimensionality", in2agg.dim),
+
+            ("tb2output_read_addr_gen_starting_addr", tb2out0.out_data_strt),
+            ("tb2output_read_sched_gen_sched_addr_gen_starting_addr", tb2out0.cyc_strt),
+            ("tb2output_forloop_dimensionality", tb2out0.dim),
+
+            ("tb12output_read_addr_gen_starting_addr", tb2out1.out_data_strt),
+            ("tb12output_read_sched_gen_sched_addr_gen_starting_addr", tb2out1.cyc_strt),
+            ("tb12output_forloop_dimensionality", tb2out1.dim),
+
+            # Control Signals...
+            ("flush_reg_sel", 0),  # 1
+            ("flush_reg_value", 0),  # 1
+
+            # Set the mode and activate the tile...
+            ("mode", 0),  # 2
+            ("tile_en", 1),  # 1
+        ]
+
+        # Check the hardware if it supports stencil valid
+        if self.stencil_valid:
+            cfg_path = config_path + '/' + 'stencil_valid.csv'
+            # Check if the stencil valid file exists...if it doesn't we just won't program it
+            if os.path.exists(cfg_path):
+                stcl_valid = map_controller(extract_controller(cfg_path), "stencil_valid")
+                config.append((f"loops_stencil_valid_dimensionality", stcl_valid.dim))
+                config.append((f"stencil_valid_sched_gen_sched_addr_gen_starting_addr", stcl_valid.cyc_strt))
+                for i in range(stcl_valid.dim):
+                    config.append((f"loops_stencil_valid_ranges_{i}", stcl_valid.extent[i]))
+                    config.append((f"stencil_valid_sched_gen_sched_addr_gen_strides_{i}", stcl_valid.cyc_stride[i]))
+            else:
+                print("No configuration file provided for stencil valid...are you expecting one to exist?")
+                print(f"Bogus stencil valid path: {cfg_path}")
+
+        # TODO: Maybe need to check if size 1?
+        for i in range(input_ports):
+            config.append((f"ren_in_{i}_reg_sel", 1))
+            config.append((f"ren_in_{i}_reg_value", 0))
+
+        for i in range(output_ports):
+            config.append((f"wen_in_{i}_reg_sel", 1))
+            config.append((f"wen_in_{i}_reg_value", 0))
+
+        for i in range(in2agg.dim):
+            config.append((f"input2agg_forloop_ranges_{i}", in2agg.extent[i]))
+            config.append((f"input2agg_write_addr_gen_strides_{i}", in2agg.in_data_stride[i]))
+            config.append((f"input2agg_write_sched_gen_sched_addr_gen_strides_{i}", in2agg.cyc_stride[i]))
+
+        for i in range(agg2sram.dim):
+            config.append((f"agg_agg1_sram_edge_read_addr_gen_strides_{i}", agg2sram.out_data_stride[i]))
+            config.append((f"agg_agg1_sram_edge_forloop_ranges_{i}", agg2sram.extent[i]))
+            config.append((f"agg_agg1_sram_edge_write_addr_gen_strides_{i}", agg2sram.in_data_stride[i]))
+            config.append((f"agg_agg1_sram_edge_sched_gen_sched_addr_gen_strides_{i}", agg2sram.cyc_stride[i]))
+
+        tbs = [tb2out0, tb2out1]
+
+        for i in range(sram2tb.dim):
+            config.append((f"sram_tb_tb1_edge_forloop_ranges_{i}", sram2tb.extent[i]))
+            config.append((f"sram_tb_tb1_edge_read_addr_gen_strides_{i}", sram2tb.out_data_stride[i]))
+            config.append((f"sram_tb_tb1_edge_sched_gen_sched_addr_gen_strides_{i}", sram2tb.cyc_stride[i]))
+            for tb in range(len(tbs)):
+                config.append((f"strg_ub_tb_write_addr_gen_{tb}_strides_{i}", sram2tb.in_data_stride[i]))
+
+        tbs = [tb2out0, tb2out1]
+        for tb in range(len(tbs)):
+            elem = tbs[tb]
+            for i in range(elem.dim):
+                config.append((f"strg_ub_loops_buf2out_read_{tb}_ranges_{i}", elem.extent[i]))
+                config.append((f"strg_ub_tb_read_addr_gen_{tb}_strides_{i}", elem.out_data_stride[i]))
+                config.append((f"strg_ub_tb_read_sched_gen_{tb}_sched_addr_gen_strides_{i}", elem.cyc_stride[i]))
+
+        return trim_config_list(flattened, config)
--- tests/test_hw_top_lake.py
+++ tests/test_hw_top_lake.py
@@ -26,6 +26,8 @@ def base_lake_tester(config_path,
                      lt_dut,
                      stencil_valid=False):
 
+    configs = lt_dut.get_static_bitstream(config_path, in_file_name, out_file_name)
+
     magma_dut = kts.util.to_magma(lt_dut,
                                   flatten_array=True,
                                   check_multiple_driver=False,
@@ -33,7 +35,6 @@ def base_lake_tester(config_path,
                                   check_flip_flop_always_ff=False)
 
     tester = fault.Tester(magma_dut, magma_dut.clk)
-    configs = []
     configs_list = []
     return lt_dut, configs, configs_list, magma_dut, tester
 
@@ -54,99 +55,103 @@ def gen_test_lake(config_path,
                          out_ports, 
                          lt_dut)
 
+    tester.circuit.clk_en = 1
     tester.circuit.clk = 0
     tester.circuit.rst_n = 0
     tester.step(2)
     tester.circuit.rst_n = 1
     tester.step(2)
-    tester.circuit.clk_en = 1
     tester.eval()
 
     # args are input ports, output ports
     in_data, out_data, valids = generate_data_lists(stream_path, in_ports, out_ports)
+
     #tester.circuit.agg1_forloop_dimensionality =
     #tester.circuit.agg1_forloop_ranges =
     #tester.circuit.agg1write_addr_gen_starting_addr =
     #tester.circuit.agg1write_addr_gen_strides =
     #tester.circuit.agg1write_sched_gen_sched_addr_gen_starting_addr =
     #tester.circuit.agg1write_sched_gen_sched_addr_gen_strides =
-    tester.circuit.agg_agg1_sram_edge_forloop_dimensionality = 2
-    tester.circuit.agg_agg1_sram_edge_forloop_ranges_0 = 4
-    tester.circuit.agg_agg1_sram_edge_forloop_ranges_1 = 16
-    tester.circuit.agg_agg1_sram_edge_read_addr_gen_starting_addr = 0
+    #tester.circuit.agg_agg1_sram_edge_forloop_dimensionality = 2
+    #tester.circuit.agg_agg1_sram_edge_forloop_ranges_0 = 4
+    #tester.circuit.agg_agg1_sram_edge_forloop_ranges_1 = 16
+    #tester.circuit.agg_agg1_sram_edge_read_addr_gen_starting_addr = 0
     # all 0
-    tester.circuit.agg_agg1_sram_edge_read_addr_gen_strides_0 = 0
+    #tester.circuit.agg_agg1_sram_edge_read_addr_gen_strides_0 = 0
 
-    tester.circuit.agg_agg1_sram_edge_sched_gen_sched_addr_gen_starting_addr = 4
-    tester.circuit.agg_agg1_sram_edge_sched_gen_sched_addr_gen_strides_0 = 4
-    tester.circuit.agg_agg1_sram_edge_sched_gen_sched_addr_gen_strides_1 = 16
+    #tester.circuit.agg_agg1_sram_edge_sched_gen_sched_addr_gen_starting_addr = 4
+    #tester.circuit.agg_agg1_sram_edge_sched_gen_sched_addr_gen_strides_0 = 4
+    #tester.circuit.agg_agg1_sram_edge_sched_gen_sched_addr_gen_strides_1 = 16
 
-    tester.circuit.agg_agg1_sram_edge_write_addr_gen_starting_addr = 0
-    tester.circuit.agg_agg1_sram_edge_write_addr_gen_strides_0 = 1
-    tester.circuit.agg_agg1_sram_edge_write_addr_gen_strides_1 = 4
+    #tester.circuit.agg_agg1_sram_edge_write_addr_gen_starting_addr = 0
+    #tester.circuit.agg_agg1_sram_edge_write_addr_gen_strides_0 = 1
+    #tester.circuit.agg_agg1_sram_edge_write_addr_gen_strides_1 = 4
 
-    tester.circuit.agg_forloop_dimensionality = 3
-    tester.circuit.agg_forloop_ranges_0 = 4 
-    tester.circuit.agg_forloop_ranges_1 = 4
-    tester.circuit.agg_forloop_ranges_2 = 16
+    #tester.circuit.agg_forloop_dimensionality = 3
+    #tester.circuit.agg_forloop_ranges_0 = 4 
+    #tester.circuit.agg_forloop_ranges_1 = 4
+    #tester.circuit.agg_forloop_ranges_2 = 16
 
-    tester.circuit.aggwrite_addr_gen_starting_addr = 0
-    tester.circuit.aggwrite_addr_gen_strides_0 = 1 
+    #tester.circuit.agg_write_addr_gen_starting_addr = 0
+    #tester.circuit.agg_write_addr_gen_strides_0 = 1 
 
-    tester.circuit.aggwrite_sched_gen_sched_addr_gen_starting_addr = 0
-    tester.circuit.aggwrite_sched_gen_sched_addr_gen_strides_0 = 1
-    tester.circuit.aggwrite_sched_gen_sched_addr_gen_strides_1 = 4
-    tester.circuit.aggwrite_sched_gen_sched_addr_gen_strides_2 = 16
+    #tester.circuit.agg_write_sched_gen_sched_addr_gen_starting_addr = 0
+    #tester.circuit.agg_write_sched_gen_sched_addr_gen_strides_0 = 1
+    #tester.circuit.agg_write_sched_gen_sched_addr_gen_strides_1 = 4
+    #tester.circuit.agg_write_sched_gen_sched_addr_gen_strides_2 = 16
 
-    tester.circuit.sram_tb_tb1_edge_forloop_dimensionality = 3
-    tester.circuit.sram_tb_tb1_edge_forloop_ranges_0 = 2
-    tester.circuit.sram_tb_tb1_edge_forloop_ranges_1 = 4
-    tester.circuit.sram_tb_tb1_edge_forloop_ranges_2 = 14
+    #tester.circuit.sram_tb_tb1_edge_forloop_dimensionality = 3
+    #tester.circuit.sram_tb_tb1_edge_forloop_ranges_0 = 2
+    #tester.circuit.sram_tb_tb1_edge_forloop_ranges_1 = 4
+    #tester.circuit.sram_tb_tb1_edge_forloop_ranges_2 = 14
 
     tester.circuit.sram_tb_tb1_edge_read_addr_gen_starting_addr = 0
-    tester.circuit.sram_tb_tb1_edge_read_addr_gen_strides_0 = 4
-    tester.circuit.sram_tb_tb1_edge_read_addr_gen_strides_1 = 1
-    tester.circuit.sram_tb_tb1_edge_read_addr_gen_strides_2 = 4
+    #tester.circuit.sram_tb_tb1_edge_read_addr_gen_strides_0 = 4
+    #tester.circuit.sram_tb_tb1_edge_read_addr_gen_strides_1 = 1
+    #tester.circuit.sram_tb_tb1_edge_read_addr_gen_strides_2 = 4
 
-    tester.circuit.sram_tb_tb1_edge_sched_gen_sched_addr_gen_starting_addr = 27
-    tester.circuit.sram_tb_tb1_edge_sched_gen_sched_addr_gen_strides_0 = 2
-    tester.circuit.sram_tb_tb1_edge_sched_gen_sched_addr_gen_strides_1 = 4
-    tester.circuit.sram_tb_tb1_edge_sched_gen_sched_addr_gen_strides_2 = 16
+    #tester.circuit.sram_tb_tb1_edge_sched_gen_sched_addr_gen_starting_addr = 27
+    #tester.circuit.sram_tb_tb1_edge_sched_gen_sched_addr_gen_strides_0 = 2
+    #tester.circuit.sram_tb_tb1_edge_sched_gen_sched_addr_gen_strides_1 = 4
+    #tester.circuit.sram_tb_tb1_edge_sched_gen_sched_addr_gen_strides_2 = 16
 
     tester.circuit.sram_tb_tb1_edge_write_addr_gen_starting_addr = 0
     tester.circuit.sram_tb_tb1_edge_write_addr_gen_strides_0 = 8
     tester.circuit.sram_tb_tb1_edge_write_addr_gen_strides_1 = 1
     tester.circuit.sram_tb_tb1_edge_write_addr_gen_strides_2 = 4
 
-    tester.circuit.tb1_forloop_dimensionality = 3
+    #tester.circuit.tb1_forloop_dimensionality = 3
     tester.circuit.tb1_forloop_ranges_0 = 4
     tester.circuit.tb1_forloop_ranges_1 = 4
     tester.circuit.tb1_forloop_ranges_2 = 14
 
-    tester.circuit.tb1read_sched_gen_sched_addr_gen_starting_addr = 32
-    tester.circuit.tb1read_sched_gen_sched_addr_gen_strides_0 = 1
-    tester.circuit.tb1read_sched_gen_sched_addr_gen_strides_1 = 4
-    tester.circuit.tb1read_sched_gen_sched_addr_gen_strides_2 = 16
+    #tester.circuit.tb1_read_sched_gen_sched_addr_gen_starting_addr = 32
+    tester.circuit.tb1_read_sched_gen_sched_addr_gen_strides_0 = 1
+    tester.circuit.tb1_read_sched_gen_sched_addr_gen_strides_1 = 4
+    tester.circuit.tb1_read_sched_gen_sched_addr_gen_strides_2 = 16
 
-    tester.circuit.tb1write_addr_gen_starting_addr = 0
-    tester.circuit.tb1write_addr_gen_strides_0 = 8
-    tester.circuit.tb1write_addr_gen_strides_1 = 1
-    tester.circuit.tb1write_addr_gen_strides_2 = 4
+    #tester.circuit.tb1_write_addr_gen_starting_addr = 0
+    tester.circuit.tb1_write_addr_gen_strides_0 = 8
+    tester.circuit.tb1_write_addr_gen_strides_1 = 1
+    tester.circuit.tb1_write_addr_gen_strides_2 = 4
 
-    tester.circuit.tb_forloop_dimensionality = 3
+    #tester.circuit.tb_forloop_dimensionality = 3
     tester.circuit.tb_forloop_ranges_0 = 4
     tester.circuit.tb_forloop_ranges_1 = 4
     tester.circuit.tb_forloop_ranges_2 = 14
 
-    tester.circuit.tbread_sched_gen_sched_addr_gen_starting_addr = 32
-    tester.circuit.tbread_sched_gen_sched_addr_gen_strides_0 = 1
-    tester.circuit.tbread_sched_gen_sched_addr_gen_strides_1 = 4
-    tester.circuit.tbread_sched_gen_sched_addr_gen_strides_2 = 16
+    #tester.circuit.tb_read_sched_gen_sched_addr_gen_starting_addr = 32
+    tester.circuit.tb_read_sched_gen_sched_addr_gen_strides_0 = 1
+    tester.circuit.tb_read_sched_gen_sched_addr_gen_strides_1 = 4
+    tester.circuit.tb_read_sched_gen_sched_addr_gen_strides_2 = 16
 
-    tester.circuit.tbwrite_addr_gen_starting_addr = 0
-    tester.circuit.tbwrite_addr_gen_strides_0 = 8
-    tester.circuit.tbwrite_addr_gen_strides_1 = 1
-    tester.circuit.tbwrite_addr_gen_strides_2 = 4
+    #tester.circuit.tb_write_addr_gen_starting_addr = 0
+    tester.circuit.tb_write_addr_gen_strides_0 = 8
+    tester.circuit.tb_write_addr_gen_strides_1 = 1
+    tester.circuit.tb_write_addr_gen_strides_2 = 4
+
+    for (f1, f2) in configs:
+        setattr(tester.circuit, f1, f2)
 
     for i in range(len(out_data[0])):
         #for j in range(len(in_data)):
