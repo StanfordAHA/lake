ec54a42ddfe68027bfbdcc0b4496a9ae352255a2
Kavya Sreedhar <skavya@stanford.edu>
2020-11-02 12:25:27 -0800

    dsl pond hw passes rd/wr tests

--- lake/collateral2compiler/pond_dsl.py
+++ lake/collateral2compiler/pond_dsl.py
@@ -8,4 +8,4 @@ write_port = MemPort(1, 0)
 pond_params = make_params("pond", 32, 1, 1)
 pond.add_memory(pond_params, read_ports=[read_port], write_ports=[write_port])
 
-pond.construct_lake()
+# pond.construct_lake()
