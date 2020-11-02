9a36dd96d5b8e961593a99bc2b3947a9984a5f73
Kavya Sreedhar <skavya@stanford.edu>
2020-11-02 13:04:03 -0800

    use edge defaults for pond for now

--- lake/collateral2compiler/pond_dsl.py
+++ lake/collateral2compiler/pond_dsl.py
@@ -6,6 +6,6 @@ read_port = MemPort(0, 0)
 write_port = MemPort(1, 0)
 
 pond_params = make_params("pond", 32, 1, 1)
-pond.add_memory(pond_params, read_ports=[read_port], write_ports=[write_port], is_input=True, is_output=True, input_edge_params=[6, 2**16, 2**16], output_edge_params=[6, 2**16, 2**16])
+pond.add_memory(pond_params, read_ports=[read_port], write_ports=[write_port])
 
 # pond.construct_lake()
