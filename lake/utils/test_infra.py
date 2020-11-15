40b3d04677bf9551381dad0a04474fe437c16a6c
Kavya Sreedhar <skavya@stanford.edu>
2020-11-15 11:58:44 -0800

    40k cycle tile level test for Joey

--- lake/utils/test_infra.py
+++ lake/utils/test_infra.py
@@ -69,25 +69,27 @@ def gen_test_lake(config_path,
     for (f1, f2) in configs:
         setattr(tester.circuit, f1, f2)
 
-    for i in range(len(out_data[0])):
-        for j in range(len(in_data)):
-            if i < len(in_data[j]):
-                setattr(tester.circuit, f"data_in_{j}", in_data[j][i])
+    for i in range(40000):
+        if i >= 10000:
+            tester.circuit.data_in_0 = 10000 - i
+        # for j in range(len(in_data)):
+        #     if i < len(in_data[j]):
+        #         setattr(tester.circuit, f"data_in_{j}", in_data[j][i])
         # tester.circuit.data_in_0 = i
 
         tester.eval()
 
         for j in range(len(out_data)):
-            if i < len(out_data[j]):
-                if len(valids) != 0 and valids[i] == 1:
-                    getattr(tester.circuit, f"data_out_{j}").expect(out_data[j][i])
+            # if i < len(out_data[j]):
+            #     if len(valids) != 0 and valids[i] == 1:
+            #         getattr(tester.circuit, f"data_out_{j}").expect(out_data[j][i])
                 if len(valids) == 0:
                     getattr(tester.circuit, f"data_out_{j}").expect(out_data[j][i])
 
         tester.step(2)
 
     with tempfile.TemporaryDirectory() as tempdir:
-        tempdir = "temp_separate"
+        tempdir = "resnet3"
         tester.compile_and_run(target="verilator",
                                directory=tempdir,
                                flags=["-Wno-fatal", "--trace"])
@@ -114,7 +116,7 @@ def conv_3_3_args():
 # This is a weird custom test path until we can get consistent paths
 def separate_args():
     clkwrk_dir = os.getenv("CLOCKWORK_DIR")
-    config_path = clkwrk_dir + "/aha_garnet_design_new/conv_3_3/lake_collateral/ub_hw_input_global_wrapper_stencil_op_hcompute_hw_input_global_wrapper_stencil_2_to_hw_input_global_wrapper_stencil_op_hcompute_conv_stencil_1_11"
+    config_path = clkwrk_dir + "/aha_garnet_design_new/resnet/lake_collateral/ub_hw_input_global_wrapper_stencil_cyclic_0_0_0"
     stream_path = clkwrk_dir + "/aha_garnet_design/conv_3_3/lake_stream/hw_input_global_wrapper_stencil_op_hcompute_hw_input_global_wrapper_stencil_2_to_hw_input_global_wrapper_stencil_op_hcompute_conv_stencil_1_11_ubuf_0_top_SMT.csv"
     in_file_name = ""
     out_file_name = ""
