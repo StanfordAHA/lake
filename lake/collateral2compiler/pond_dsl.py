c16b951064364afb0fe8eb112d727e0bfabd0cd8
Kavya Sreedhar <skavya@stanford.edu>
2020-11-18 16:10:06 -0800

    pond verilog

--- lake/collateral2compiler/pond_dsl.py
+++ lake/collateral2compiler/pond_dsl.py
@@ -6,6 +6,6 @@ read_port = MemPort(0, 0)
 write_port = MemPort(1, 0)
 
 pond_params = make_params("pond", 32, 1, 1)
-pond.add_memory(pond_params, read_ports=[read_port], write_ports=[write_port])
+pond.add_memory(pond_params, read_ports=[read_port], write_ports=[write_port], is_input=True, input_edge_params=[2, 2**5, 2**5], is_output=True, output_edge_params=[2, 2**5, 2**5])
 
 # pond.construct_lake()
