931532f4a8f3aaabd8584535536428d7cf6c1f86
Kavya Sreedhar <skavya@stanford.edu>
2020-11-19 17:21:54 -0800

    simba macros

--- lake/collateral2compiler/simba_weights.py
+++ lake/collateral2compiler/simba_weights.py
@@ -3,32 +3,29 @@ from lake.collateral2compiler.lake_imports import *
 # weight buffer
 weights = TopLake(64, 1, 1)
 
-read_port = MemPort(0, 0)
-write_port = MemPort(1, 0)
+port = MemPort(1, 0)
 
 weights_params = make_params("weights", 512, 1, 1)
-weights.add_memory(weights_params, read_ports=[read_port], write_ports=[write_port])
+weights.add_memory(weights_params, read_write_ports=[port])
 
 weights.construct_lake("simba_weights.sv")
 
 # accumulation buffer
 accum = TopLake(8*24, 1, 1)
 
-read_port = MemPort(0, 0)
-write_port = MemPort(1, 0)
+port = MemPort(1, 0)
 
 accum_params = make_params("accum", 64, 1, 1)
-accum.add_memory(accum_params, read_ports=[read_port], write_ports=[write_port])
+accum.add_memory(accum_params, read_write_ports=[port])
 
 accum.construct_lake("simba_accum.sv")
 
 # input activation buffer
 input_buffer = TopLake(64, 1, 1)
 
-read_port = MemPort(0, 0)
-write_port = MemPort(1, 0)
+port = MemPort(1, 0)
 
 input_params = make_params("input_buffer", 1024, 1, 1)
-input_buffer.add_memory(weights_params, read_ports=[read_port], write_ports=[write_port])
+input_buffer.add_memory(weights_params, read_write_ports[port])
 
 input_buffer.construct_lake("simba_input.sv")
