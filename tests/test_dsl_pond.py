114478fa2f425d3e6e9dfa1951b1ddfe91772c3b
Kavya Sreedhar <skavya@stanford.edu>
2020-12-23 17:29:11 -0800

    add last pond test from master, passes

--- tests/test_dsl_pond.py
+++ tests/test_dsl_pond.py
@@ -183,5 +183,60 @@ def test_pond_b2b_read(data_width=16,  # CGRA Params
                                flags=["-Wno-fatal"])
 
 
+def test_pond_double_buffer(data_width=16,  # CGRA Params
+                            mem_depth=32,
+                            default_iterator_support=2,
+                            config_data_width=32,
+                            config_addr_width=8,
+                            cycle_count_width=16,
+                            add_clk_enable=True,
+                            add_flush=True,
+                            interconnect_input_ports=1,  # Connection to int
+                            interconnect_output_ports=1):
+
+    # Ranges, Strides, Dimensionality, Starting Addr, Starting Addr - Schedule
+    ctrl_rd = [[32, 1], [1, 0], 2, 0, 16, [1, 0]]
+    ctrl_wr = [[32, 1], [1, 1], 2, 0, 0, [1, 1]]
+    pond_config = generate_pond_api(ctrl_rd, ctrl_wr, True)
+
+    for key, value in pond_config.items():
+        setattr(tester.circuit, key, value)
+
+    rand.seed(0)
+    tester.circuit.clk = 0
+    tester.circuit.clk_en = 1
+    tester.circuit.tile_en = 1
+    tester.circuit.rst_n = 0
+    tester.step(1)
+    tester.circuit.rst_n = 1
+    tester.step(1)
+    tester.circuit.clk_en = 1
+
+    data_in_pond = [0] * interconnect_input_ports
+    valid_in = [0] * interconnect_input_ports
+    for i in range(48):
+        # Incrementing Data
+        if i < 32:
+            data_in_pond[0] = data_in_pond[0] + 1
+
+        if interconnect_input_ports == 1:
+            tester.circuit.data_in = data_in_pond[0]
+        else:
+            for j in range(interconnect_input_ports):
+                setattr(tester.circuit, f"data_in_{j}", data_in_pond[j])
+
+        if i >= 16:
+            tester.circuit.data_out.expect(i - 15)
+
+        tester.eval()
+        tester.step(2)
+
+    with tempfile.TemporaryDirectory() as tempdir:
+        tester.compile_and_run(target="verilator",
+                               directory=tempdir,
+                               magma_output="verilog",
+                               flags=["-Wno-fatal"])
+
+
 if __name__ == "__main__":
     test_pond_strided_read()
