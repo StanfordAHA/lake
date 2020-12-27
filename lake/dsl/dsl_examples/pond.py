ba18928616c6b29958a6cee3a2db1abf99cf190d
Kavya Sreedhar <skavya@stanford.edu>
2020-12-26 18:35:45 -0800

    add name to pond sv file

--- lake/dsl/dsl_examples/pond.py
+++ lake/dsl/dsl_examples/pond.py
@@ -10,4 +10,4 @@ pond.add_memory(pond_params, read_ports=[read_port], write_ports=[write_port])
 pond.add_input_edge(0, "pond", dim=2, max_stride=2**5)
 pond.add_output_edge(0, "pond", dim=2, max_stride=2**5)
 
-# pond.construct_lake()
+# pond.construct_lake("pond.sv")
