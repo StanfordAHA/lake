7a19c3e9788baba174de54ffb65916371aeb80b6
Kavya Sreedhar <skavya@stanford.edu>
2021-01-15 17:27:38 -0800

    simpler way to write memtile, test passing

--- lake/dsl/dsl_examples/memtile.py
+++ lake/dsl/dsl_examples/memtile.py
@@ -1,8 +1,10 @@
 from lake.dsl.lake_imports import *
 
+# Amber memory tile: 2 aggs -> SRAM -> 2 tbs
 tile = Lake(16, 2, 2)
 
 fw = 4  # fetch_width for SRAM
+num_aggs, num_tbs = 2, 2
 
 # wide fetch SRAM
 sram_port = MemPort(1, 1)
@@ -10,10 +12,8 @@ sram_params = make_params("sram", 512, read_write_port_width=fw)
 tile.add_memory(sram_params, read_write_ports=[sram_port])
 
 # aggregators: serial to parallel converters before SRAM
-for i in range(2):
+for i in range(num_aggs):
     mem_name = f"agg{i}"
-    if i == 0:
-        mem_name = "agg"
     write_port, read_port = MemPort(1, 1), MemPort(0, 1)
     agg_params = make_params(mem_name,
                              fw,
@@ -28,10 +28,8 @@ for i in range(2):
                   max_range=65536)
 
 # transpose buffers: parallel to serial converters after SRAM
-for i in range(2):
+for i in range(num_tbs):
     mem_name = f"tb{i}"
-    if i == 0:
-        mem_name = "tb"
     write_port, read_port = MemPort(1, 1), MemPort(0, 1)
     tb_params = make_params(mem_name,
                             fw * 2,  # double buffer
