f67f2b30e4ed39964ce886071e84fb48d60848e3
mbstrange2 <mstrange@stanford.edu>
2021-01-27 15:58:44 -0800

    cleanup and some extra robust logic for scanner

--- lake/modules/scanner.py
+++ lake/modules/scanner.py
@@ -3,7 +3,7 @@ from kratos import *
 from lake.passes.passes import lift_config_reg
 from lake.modules.for_loop import ForLoop
 from lake.modules.addr_gen import AddrGen
-from lake.utils.util import safe_wire, trim_config_list
+from lake.utils.util import add_counter, safe_wire, trim_config_list
 from lake.attributes.formal_attr import FormalAttr, FormalSignalConstraint
 from lake.attributes.config_reg_attr import ConfigRegAttr
 from lake.attributes.control_signal_attr import ControlSignalAttr
@@ -46,6 +46,9 @@ class Scanner(Generator):
         self._valid_in = self.input("valid_in", 1)
         self._valid_in.add_attribute(ControlSignalAttr(is_control=True))
 
+        self._ready_in = self.input("ready_in", 1)
+        self._ready_in.add_attribute(ControlSignalAttr(is_control=True))
+
         self._ready_out = self.output("ready_out", 1)
         self._ready_out.add_attribute(ControlSignalAttr(is_control=False))
         
@@ -54,9 +57,6 @@ class Scanner(Generator):
 
         self._valid_out = self.output("valid_out", 1)
         self._valid_out.add_attribute(ControlSignalAttr(is_control=False))
-
-        self._ready_in = self.input("ready_in", 1)
-        self._ready_in.add_attribute(ControlSignalAttr(is_control=True))
 
         self._addr_out = self.output("addr_out", self.data_width)
         self._addr_out.add_attribute(ControlSignalAttr(is_control=False, full_bus=True))
@@ -72,13 +72,13 @@ class Scanner(Generator):
 # ==========================================
 
         # Create read address generator
-        FIBER_READ_ITER = ForLoop(iterator_support=2,
+        self.FIBER_READ_ITER = ForLoop(iterator_support=2,
                                 config_width=16)
-        FIBER_READ_ADDR = AddrGen(iterator_support=2,
+        self.FIBER_READ_ADDR = AddrGen(iterator_support=2,
                                 config_width=16)
 
         self.add_child(f"fiber_read_iter",
-                        FIBER_READ_ITER,
+                        self.FIBER_READ_ITER,
                         clk=self._gclk,
                         rst_n=self._rst_n,
                         step=self._valid_in)
@@ -88,13 +88,16 @@ class Scanner(Generator):
 
         # finished=RF_WRITE_ITER.ports.restart,
         self.add_child(f"fiber_read_addr",
-                        FIBER_READ_ADDR,
+                        self.FIBER_READ_ADDR,
                         clk=self._gclk,
                         rst_n=self._rst_n,
                         step=self._valid_in,
-                        mux_sel=FIBER_READ_ITER.ports.mux_sel_out,
-                        restart=FIBER_READ_ITER.ports.restart)
-        safe_wire(self, self._addr_out, FIBER_READ_ADDR.ports.addr_out)
+                        mux_sel=self.FIBER_READ_ITER.ports.mux_sel_out,
+                        restart=self.FIBER_READ_ITER.ports.restart)
+        safe_wire(self, self._addr_out, self.FIBER_READ_ADDR.ports.addr_out)
+
+        self._iter_restart = self.var("iter_restart", 1)
+        self.wire(self._iter_restart, self.FIBER_READ_ITER.ports.restart)
 
 # ===================================
 # Dump metadata into fifo
@@ -102,14 +105,27 @@ class Scanner(Generator):
 
         # Stupid convert -
         self._data_in_packed = self.var("fifo_in_packed", self.data_width + 1, packed=True)
-        self.wire(self._data_in_packed[self.data_width], FIBER_READ_ITER.ports.restart)
+        self._last_valid_accepting = self.var("last_valid_accepting", 1)
+        # The EOS tags on the last valid in the stream
+        self.wire(self._data_in_packed[self.data_width], self._last_valid_accepting)
         self.wire(self._data_in_packed[self.data_width - 1, 0], self._data_in)
 
         self._data_out_packed = self.var("fifo_out_packed", self.data_width + 1, packed=True)
         self.wire(self._eos_out, self._data_out_packed[self.data_width])
         self.wire(self._data_out, self._data_out_packed[self.data_width - 1, 0])
 
-        self._rfifo = RegFIFO(data_width=self.data_width + 1, width_mult=1, depth=8)
+        self._rfifo = RegFIFO(data_width=self.data_width + 1, width_mult=1, depth=16)
+
+        # Gate ready after last read in the stream
+        self._ready_gate = self.var("ready_gate", 1)
+        @always_ff((posedge, "clk"), (negedge, "rst_n"))
+        def ready_gate_ff():
+            if ~self._rst_n:
+                self._ready_gate = 0
+            elif self._iter_restart:
+                self._ready_gate = 1
+        self.add_code(ready_gate_ff)
+
         self.add_child(f"coordinate_fifo",
                        self._rfifo,
                        clk=self._gclk,
@@ -120,7 +136,20 @@ class Scanner(Generator):
                        data_in=self._data_in_packed,
                        data_out=self._data_out_packed,
                        valid=self._valid_out)
-        self.wire(self._ready_out, ~self._rfifo.ports.full)
+        self.wire(self._ready_out, ~self._rfifo.ports.full & ~self._ready_gate)
+
+        self._fifo_full = self.var("fifo_full", 1)
+        self.wire(self._fifo_full, self._rfifo.ports.full)
+        # Increment valid count when we receive a valid we can actually push in the FIFO
+        self._valid_cnt = add_counter(self, "valid_count", 16, self._valid_in & (~self._fifo_full))
+
+        self._stream_length = self.input("stream_length", 16)
+        self._stream_length.add_attribute(ConfigRegAttr("How long is the stream..."))
+
+        @always_comb
+        def eos_comparison():
+            self._last_valid_accepting = (self._valid_cnt == self._stream_length) & (self._valid_in)
+        self.add_code(eos_comparison)
 
         if self.add_clk_enable:
             # self.clock_en("clk_en")
@@ -137,25 +166,25 @@ class Scanner(Generator):
         # Finally, lift the config regs...
         lift_config_reg(self.internal_generator)
 
-    def get_static_bitstream(self,
-                                  length):
+    def get_bitstream(self, length):
 
         # Store all configurations here
-        config = []
+        config = [("fiber_read_addr_starting_addr", 0),
+                  ("fiber_read_iter_dimensionality", 1),
+                  ("fiber_read_addr_strides_0", 1),
+                #   ("fiber_read_addr_strides_1", 0),
+                  ("fiber_read_iter_ranges_0", length - 2),
+                  ("fiber_read_iter_ranges_1", 0),
+                  ("stream_length", length - 1)
+                  ]
         # Dummy variables to fill in later when compiler
         # generates different collateral for different designs
-        flattened = create_wrapper_flatten(self.internal_generator.clone(),
-                                           self.name + "_W")
-        config.append(("scanner_fiber_read_iter_starting_address", 0))
-        config.append(("strg_ub_agg_only_agg_write_sched_gen_1_enable", 1))
-        config.append(("strg_ub_agg_only_loops_in2buf_1_dimensionality", 1))
-        config.append((f"strg_ub_agg_only_loops_in2buf_1_ranges_0", length))
-        config.append((f"strg_ub_agg_only_agg_write_addr_gen_1_strides_0", 1))
+        # flattened = create_wrapper_flatten(self.internal_generator.clone(),
+        #                                    self.name + "_W")
 
-
-
-        # Trim the list
-        return trim_config_list(flattened, config)
+        # # Trim the list
+        # return trim_config_list(flattened, config)
+        return config
 
 
 if __name__ == "__main__":
