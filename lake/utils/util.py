0b62ff67c6e7882c33c7726359613d6968b3d8a7
Ankita Nayak <ankita0805@gmail.com>
2021-02-01 12:18:22 -0800

    add function to get api from csv

--- lake/utils/util.py
+++ lake/utils/util.py
@@ -5,7 +5,7 @@ import math
 import os as os
 from enum import Enum
 from lake.attributes.formal_attr import *
-
+import csv
 
 lake_util_verbose_trim = False
 
@@ -498,6 +498,43 @@ def add_config_reg(generator, name, description, bitwidth, **kwargs):
     cfg_reg = generator.input(name, bitwidth, **kwargs)
     cfg_reg.add_attribute(ConfigRegAttr(description))
     return cfg_reg
+
+
+
+# Ranges, Strides, Dimensionality, Starting Addr
+# Starting Addr (schedule), Ranges (schedule)
+#ctrl_rd = [[16, 1], [1, 1], 2, 0, 16, [1, 1]]
+#ctrl_wr = [[16, 1], [1, 1], 2, 0, 0, [1, 1]]
+
+def get_pond_configs(config_file):
+
+    # Ranges, Strides, Dimensionality, Starting Addr
+    # Starting Addr (schedule), Ranges (schedule)
+    #ctrl_rd = [[16, 1], [1, 1], 2, 0, 16, [1, 1]]
+    #ctrl_wr = [[16, 1], [1, 1], 2, 0, 0, [1, 1]]
+
+    configs = {} 
+    with open(config_file) as csvfile:
+        config_data = csv.reader(csvfile, delimiter=',')
+        for row in config_data:
+            configs[row[0]] = int(row[1])
+    ctrl_rd = [[configs["rf_read_iter_0_ranges_0"], configs["rf_read_iter_0_ranges_1"]],
+               [configs["rf_read_addr_0_strides_0"], configs["rf_read_addr_0_strides_1"]],
+               configs["rf_read_iter_0_dimensionality"], 
+               configs["rf_read_addr_0_starting_addr"],
+               configs["rf_read_sched_0_sched_addr_gen_starting_addr"], 
+               [configs["rf_read_sched_0_sched_addr_gen_strides_0"], configs["rf_read_sched_0_sched_addr_gen_strides_1"]]]
+            
+
+    ctrl_wr = [[configs["rf_write_iter_0_ranges_0"], configs["rf_write_iter_0_ranges_1"]],
+               [configs["rf_write_addr_0_strides_0"], configs["rf_write_addr_0_strides_1"]],
+               configs["rf_write_iter_0_dimensionality"],
+               configs["rf_write_addr_0_starting_addr"],
+               configs["rf_write_sched_0_sched_addr_gen_starting_addr"],
+               [configs["rf_write_sched_0_sched_addr_gen_strides_0"], configs["rf_write_sched_0_sched_addr_gen_strides_1"]]]
+
+    print(ctrl_rd, ctrl_wr)
+    return ctrl_rd, ctrl_wr 
 
 
 # Function for generating Pond API
