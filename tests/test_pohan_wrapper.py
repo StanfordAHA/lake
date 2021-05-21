679e1886e2a3a13808dddfe54504f9b06e9bcae5
mbstrange2 <mstrange@stanford.edu>
2021-05-21 12:25:58 -0700

    add argparse for J

--- tests/test_pohan_wrapper.py
+++ tests/test_pohan_wrapper.py
@@ -1,19 +1,26 @@
 from lake.top.pohan_top import PohanTop
-from lake.utils.wrapper import get_dut, wrapper
 from kratos import *
 import pytest
+import argparse
 
 
 @pytest.mark.skip
-def test_gen_dual_port():
+def test_gen_dual_port(config_path="/aha/config.json"):
 
-    config_path = "/aha/config.json"
-
+    print(f"Using configuration file at: {config_path}")
     pohan_top = PohanTop()
     pohan_top_wrapper = pohan_top.wrapper(module_name="joey_mod",
                                           config_path=config_path,
                                           name="pohan_wrapper")
     return pohan_top_wrapper
 
+
 if __name__ == "__main__":
-    test_gen_dual_port()
+    ap = argparse.ArgumentParser(description="Parse arguments for PohanTop")
+    ap.add_argument("-f",
+                    type=str,
+                    help="required: path to configuration json file",
+                    default="/aha/config.json")
+    args = ap.parse_args()
+
+    test_gen_dual_port(config_path=args.f)
