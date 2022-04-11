feb1d045ce38ad8bb0d48f3f39c791415b086f81
Maxwell Strange <mstrange@stanford.edu>
2022-04-11 10:36:40 -0700

    add generation argparse

--- lake/top/lake_top.py
+++ lake/top/lake_top.py
@@ -345,23 +345,34 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='LakeTop')
     parser.add_argument("-f",
                         help="optional: will generate verilog, annotation file, and dim to strides/range mapping collateral to solve a formal problem. must provide module to solve for")
+    parser.add_argument("--fetch_width", type=int, default=4)
+    parser.add_argument("--dual_port", action="store_true")
 
     args = parser.parse_args()
 
+    mem_width = args.fetch_width * 16
+
+    mem_name = "single"
+    rw_same_cycle = False
+    if args.dual_port:
+        rw_same_cycle = True
+        mem_name = "dual"
+
     lake_top = LakeTop(data_width=16,
-                       mem_width=64,
+                       mem_width=mem_width,
                        mem_depth=512,
                        banks=1,
                        fifo_mode=True,
                        add_clk_enable=True,
                        add_flush=True,
                        rw_same_cycle=False,
-                       read_delay=1)
+                       read_delay=1,
+                       name=f"LakeTop_width_{args.fetch_width}_{mem_name}")
 
     print(lake_top)
 
     config = extract_top_config(lake_top.dut, verbose=True)
 
     # generate verilog
-    verilog(lake_top.dut, filename=f"lake_top_test.sv",
+    verilog(lake_top.dut, filename=f"LakeTop_width_{args.fetch_width}_{mem_name}.sv",
             optimize_if=False)
