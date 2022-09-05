521bdcef05772eb803ab1c9c23b704cbd5ebb4c9
mbstrange2 <mstrange@stanford.edu>
2022-09-05 13:05:19 -0700

    @kuree - bug fix - drop stop tokens on empty child in crddrop
    lake: this
    garnet: c7ae00bce7dfec26f8ebf0301ed343e2f945c379
    sam: 042f86ae94dda956e0e3bb0748e383fced8b3eff
    gemstone: 28b10a6dc352d1ea46377d9e76336f6fb4265953
    canal: e85910b93619d4c229f27805713775cc7a9424ae
    
    python tests/test_memory_core/build_tb.py --ic_fork --sam_graph ../sam/compiler/sam-outputs/dot/tensor3_elemmul.gv --output_dir /home/max/Documents/SPARSE/garnet/mek_outputs/ --input_dir /home/max/Documents/SPARSE/garnet/final_mat/ --test_dump_dir ./mek_dump --matrix_tmp_dir tmp_mat/ --seed 1 --trace --gold_dir gold_out/ --fifo_depth 8 --gen_pe --dump_bitstream --add_pond --combined --pipeline_scanner

--- lake/modules/crddrop.py
+++ lake/modules/crddrop.py
@@ -346,12 +346,16 @@ class CrdDrop(MemoryController):
                                                                                     ~proc_outfifo.ports.full,
                                                                                     0))))
         # The push is basically the same as the pop
+        # But we only want to push the STOP token if we have previously pushed data on the line, otherwise we are dropping the fiber from both
+        # levels of the hierarchy
         PROCESS.output(self._cmrg_fifo_push[0], kts.ternary(self._base_done,
                                                             self._proc_done,
                                                             kts.ternary(self._base_infifo_in_valid & ~self._base_infifo_in_eos,
                                                                         ~base_outfifo.ports.full,
                                                                         kts.ternary(self._base_infifo_in_valid & self._base_infifo_in_eos,
-                                                                                    self._proc_infifo_in_valid & ~self._proc_infifo_in_eos & ~base_outfifo.ports.full & ~proc_outfifo.ports.full,
+                                                                                    # self._proc_infifo_in_valid & ~self._proc_infifo_in_eos & ~base_outfifo.ports.full & ~proc_outfifo.ports.full,
+                                                                                    self._proc_infifo_in_valid & ~self._proc_infifo_in_eos & ~base_outfifo.ports.full &
+                                                                                        self._pushed_data_lower & ~proc_outfifo.ports.full,
                                                                                     0))))
         # Push is similar to pop, but in the case of a real data on proc, we only push it if we pushed a data on the base level
         PROCESS.output(self._cmrg_fifo_push[1], kts.ternary(self._proc_done,
