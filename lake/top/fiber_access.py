30f6e841edf3004b911a6333fb2638a09ccb272a
mbstrange2 <mstrange@stanford.edu>
2022-09-08 14:54:53 -0700

    update fiber access for use in tests

--- lake/top/fiber_access.py
+++ lake/top/fiber_access.py
@@ -4,6 +4,7 @@ from lake.attributes.control_signal_attr import ControlSignalAttr
 from lake.top.memory_controller import MemoryController
 from lake.modules.buffet_like import BuffetLike
 from lake.modules.scanner import Scanner
+from lake.modules.scanner_pipe import ScannerPipe
 from lake.modules.write_scanner import WriteScanner
 from lake.passes.passes import lift_config_reg
 from lake.top.tech_maps import GF_Tech_Map
@@ -14,15 +15,22 @@ class FiberAccess(MemoryController):
     def __init__(self, data_width=16,
                  local_memory=True,
                  tech_map=GF_Tech_Map(depth=512, width=32),
-                 defer_fifos=True):
+                 defer_fifos=True,
+                 use_pipelined_scanner=False,
+                 add_flush=False):
         super().__init__(f'fiber_access_{data_width}', debug=True)
+
+        self.wr_scan_pre = "write_scanner"
+        self.rd_scan_pre = "read_scanner"
+        self.buffet_pre = "buffet"
 
         self.data_width = data_width
         self.add_clk_enable = True
-        self.add_flush = True
+        self.add_flush = add_flush
         self.local_memory = local_memory
         self.tech_map = tech_map
         self.defer_fifos = defer_fifos
+        self.use_pipelined_scanner = use_pipelined_scanner
 
         # inputs
         self._clk = self.clock("clk")
@@ -37,39 +45,39 @@ class FiberAccess(MemoryController):
         self._gclk = kts.util.clock(gclk)
         self.wire(gclk, kts.util.clock(self._clk & self._tile_en))
 
-        self._wr_scan_data_in = self.input("wr_scan_data_in", self.data_width + 1, packed=True)
+        self._wr_scan_data_in = self.input(f"{self.wr_scan_pre}_data_in", self.data_width + 1, packed=True)
         self._wr_scan_data_in.add_attribute(ControlSignalAttr(is_control=False, full_bus=True))
-        self._wr_scan_data_in_ready = self.output("wr_scan_data_in_ready", 1)
+        self._wr_scan_data_in_ready = self.output(f"{self.wr_scan_pre}_data_in_ready", 1)
         self._wr_scan_data_in_ready.add_attribute(ControlSignalAttr(is_control=False, full_bus=False))
-        self._wr_scan_data_in_valid = self.input("wr_scan_data_in_valid", 1)
+        self._wr_scan_data_in_valid = self.input(f"{self.wr_scan_pre}_data_in_valid", 1)
         self._wr_scan_data_in_valid.add_attribute(ControlSignalAttr(is_control=True, full_bus=False))
 
-        self._wr_scan_addr_in = self.input("wr_scan_addr_in", self.data_width + 1, packed=True)
+        self._wr_scan_addr_in = self.input(f"{self.wr_scan_pre}_addr_in", self.data_width + 1, packed=True)
         self._wr_scan_addr_in.add_attribute(ControlSignalAttr(is_control=False, full_bus=True))
-        self._wr_scan_addr_in_ready = self.output("wr_scan_addr_in_ready", 1)
+        self._wr_scan_addr_in_ready = self.output(f"{self.wr_scan_pre}_addr_in_ready", 1)
         self._wr_scan_addr_in_ready.add_attribute(ControlSignalAttr(is_control=False, full_bus=False))
-        self._wr_scan_addr_in_valid = self.input("wr_scan_addr_in_valid", 1)
+        self._wr_scan_addr_in_valid = self.input(f"{self.wr_scan_pre}_addr_in_valid", 1)
         self._wr_scan_addr_in_valid.add_attribute(ControlSignalAttr(is_control=True, full_bus=False))
 
-        self._rd_scan_coord_out = self.output("rd_scan_coord_out", self.data_width + 1, packed=True)
+        self._rd_scan_coord_out = self.output(f"{self.rd_scan_pre}_coord_out", self.data_width + 1, packed=True)
         self._rd_scan_coord_out.add_attribute(ControlSignalAttr(is_control=False, full_bus=True))
-        self._rd_scan_coord_out_ready = self.input("rd_scan_coord_out_ready", 1)
+        self._rd_scan_coord_out_ready = self.input(f"{self.rd_scan_pre}_coord_out_ready", 1)
         self._rd_scan_coord_out_ready.add_attribute(ControlSignalAttr(is_control=True, full_bus=False))
-        self._rd_scan_coord_out_valid = self.output("rd_scan_coord_out_valid", 1)
+        self._rd_scan_coord_out_valid = self.output(f"{self.rd_scan_pre}_coord_out_valid", 1)
         self._rd_scan_coord_out_valid.add_attribute(ControlSignalAttr(is_control=False, full_bus=False))
 
-        self._rd_scan_pos_out = self.output("rd_scan_pos_out", self.data_width + 1, packed=True)
+        self._rd_scan_pos_out = self.output(f"{self.rd_scan_pre}_pos_out", self.data_width + 1, packed=True)
         self._rd_scan_pos_out.add_attribute(ControlSignalAttr(is_control=False, full_bus=True))
-        self._rd_scan_pos_out_ready = self.input("rd_scan_pos_out_ready", 1)
+        self._rd_scan_pos_out_ready = self.input(f"{self.rd_scan_pre}_pos_out_ready", 1)
         self._rd_scan_pos_out_ready.add_attribute(ControlSignalAttr(is_control=True, full_bus=False))
-        self._rd_scan_pos_out_valid = self.output("rd_scan_pos_out_valid", 1)
+        self._rd_scan_pos_out_valid = self.output(f"{self.rd_scan_pre}_pos_out_valid", 1)
         self._rd_scan_pos_out_valid.add_attribute(ControlSignalAttr(is_control=False, full_bus=False))
 
-        self._rd_scan_us_pos_in = self.input("rd_scan_us_pos_in", self.data_width + 1, packed=True)
+        self._rd_scan_us_pos_in = self.input(f"{self.rd_scan_pre}_us_pos_in", self.data_width + 1, packed=True)
         self._rd_scan_us_pos_in.add_attribute(ControlSignalAttr(is_control=False, full_bus=True))
-        self._rd_scan_us_pos_in_ready = self.output("rd_scan_us_pos_in_ready", 1)
+        self._rd_scan_us_pos_in_ready = self.output(f"{self.rd_scan_pre}_us_pos_in_ready", 1)
         self._rd_scan_us_pos_in_ready.add_attribute(ControlSignalAttr(is_control=False, full_bus=False))
-        self._rd_scan_us_pos_in_valid = self.input("rd_scan_us_pos_in_valid", 1)
+        self._rd_scan_us_pos_in_valid = self.input(f"{self.rd_scan_pre}_us_pos_in_valid", 1)
         self._rd_scan_us_pos_in_valid.add_attribute(ControlSignalAttr(is_control=True, full_bus=False))
 
         self.buffet = BuffetLike(data_width=self.data_width, num_ID=2, mem_depth=512,
@@ -77,82 +85,95 @@ class FiberAccess(MemoryController):
                                  tech_map=self.tech_map,
                                  defer_fifos=self.defer_fifos)
 
-        wr_scan = WriteScanner(data_width=self.data_width,
-                               defer_fifos=self.defer_fifos)
+        self.wr_scan = WriteScanner(data_width=self.data_width,
+                                    defer_fifos=self.defer_fifos)
 
-        rd_scan = Scanner(data_width=self.data_width,
-                          defer_fifos=self.defer_fifos)
+        if self.use_pipelined_scanner:
+            self.rd_scan = ScannerPipe(data_width=self.data_width,
+                                       defer_fifos=self.defer_fifos)
+        else:
+            self.rd_scan = Scanner(data_width=self.data_width,
+                                   defer_fifos=self.defer_fifos)
 
-        self.add_child('buffet_like',
+        self.add_child(self.buffet_pre,
                        self.buffet,
                        clk=self._gclk,
                        rst_n=self._rst_n,
-                       clk_en=self._clk_en,
-                       tile_en=self._tile_en)
+                       clk_en=self._clk_en)
 
-        self.add_child('wr_scan',
-                       wr_scan,
+        self.add_child(self.wr_scan_pre,
+                       self.wr_scan,
                        clk=self._gclk,
                        rst_n=self._rst_n,
-                       clk_en=self._clk_en,
-                       tile_en=self._tile_en)
+                       clk_en=self._clk_en)
 
-        self.add_child('rd_scan',
-                       rd_scan,
+        self.add_child(self.rd_scan_pre,
+                       self.rd_scan,
                        clk=self._gclk,
                        rst_n=self._rst_n,
-                       clk_en=self._clk_en,
-                       tile_en=self._tile_en)
+                       clk_en=self._clk_en)
+
+        if self.use_pipelined_scanner:
+            self._rd_scan_block_rd_out = self.output(f"{self.rd_scan_pre}_block_rd_out", self.data_width + 1, packed=True)
+            self._rd_scan_block_rd_out.add_attribute(ControlSignalAttr(is_control=False, full_bus=True))
+            self._rd_scan_block_rd_out_ready = self.input(f"{self.rd_scan_pre}_block_rd_out_ready", 1)
+            self._rd_scan_block_rd_out_ready.add_attribute(ControlSignalAttr(is_control=True, full_bus=False))
+            self._rd_scan_block_rd_out_valid = self.output(f"{self.rd_scan_pre}_block_rd_out_valid", 1)
+            self._rd_scan_block_rd_out_valid.add_attribute(ControlSignalAttr(is_control=False, full_bus=False))
+
+            self.wire(self._rd_scan_block_rd_out, self.rd_scan.ports.block_rd_out)
+            self.wire(self._rd_scan_block_rd_out_ready, self.rd_scan.ports.block_rd_out_ready)
+            self.wire(self._rd_scan_block_rd_out_valid, self.rd_scan.ports.block_rd_out_valid)
 
         # Now wire everything
         # buffet to wr_scan
-        self.wire(self.buffet.ports.wr_ID, wr_scan.ports.ID_out)
-        self.wire(self.buffet.ports.wr_ID_ready, wr_scan.ports.ID_out_ready)
-        self.wire(self.buffet.ports.wr_ID_valid, wr_scan.ports.ID_out_valid)
+        self.wire(self.buffet.ports.wr_ID, self.wr_scan.ports.ID_out)
+        self.wire(self.buffet.ports.wr_ID_ready, self.wr_scan.ports.ID_out_ready)
+        self.wire(self.buffet.ports.wr_ID_valid, self.wr_scan.ports.ID_out_valid)
 
-        self.wire(self.buffet.ports.wr_addr, wr_scan.ports.addr_out)
-        self.wire(self.buffet.ports.wr_addr_ready, wr_scan.ports.addr_out_ready)
-        self.wire(self.buffet.ports.wr_addr_valid, wr_scan.ports.addr_out_valid)
+        self.wire(self.buffet.ports.wr_addr, self.wr_scan.ports.addr_out)
+        self.wire(self.buffet.ports.wr_addr_ready, self.wr_scan.ports.addr_out_ready)
+        self.wire(self.buffet.ports.wr_addr_valid, self.wr_scan.ports.addr_out_valid)
 
-        self.wire(self.buffet.ports.wr_data, wr_scan.ports.data_out)
-        self.wire(self.buffet.ports.wr_data_ready, wr_scan.ports.data_out_ready)
-        self.wire(self.buffet.ports.wr_data_valid, wr_scan.ports.data_out_valid)
+        self.wire(self.buffet.ports.wr_data, self.wr_scan.ports.data_out)
+        self.wire(self.buffet.ports.wr_data_ready, self.wr_scan.ports.data_out_ready)
+        self.wire(self.buffet.ports.wr_data_valid, self.wr_scan.ports.data_out_valid)
 
-        self.wire(self.buffet.ports.rd_op, rd_scan.ports.op_out)
-        self.wire(self.buffet.ports.rd_op_ready, rd_scan.ports.op_out_ready)
-        self.wire(self.buffet.ports.rd_op_valid, rd_scan.ports.op_out_valid)
+        self.wire(self.buffet.ports.rd_op, self.rd_scan.ports.op_out)
+        self.wire(self.buffet.ports.rd_op_ready, self.rd_scan.ports.op_out_ready)
+        self.wire(self.buffet.ports.rd_op_valid, self.rd_scan.ports.op_out_valid)
 
-        self.wire(self.buffet.ports.rd_addr, rd_scan.ports.addr_out)
-        self.wire(self.buffet.ports.rd_addr_ready, rd_scan.ports.addr_out_ready)
-        self.wire(self.buffet.ports.rd_addr_valid, rd_scan.ports.addr_out_valid)
+        self.wire(self.buffet.ports.rd_addr, self.rd_scan.ports.addr_out)
+        self.wire(self.buffet.ports.rd_addr_ready, self.rd_scan.ports.addr_out_ready)
+        self.wire(self.buffet.ports.rd_addr_valid, self.rd_scan.ports.addr_out_valid)
 
-        self.wire(self.buffet.ports.rd_ID, rd_scan.ports.ID_out)
-        self.wire(self.buffet.ports.rd_ID_ready, rd_scan.ports.ID_out_ready)
-        self.wire(self.buffet.ports.rd_ID_valid, rd_scan.ports.ID_out_valid)
+        self.wire(self.buffet.ports.rd_ID, self.rd_scan.ports.ID_out)
+        self.wire(self.buffet.ports.rd_ID_ready, self.rd_scan.ports.ID_out_ready)
+        self.wire(self.buffet.ports.rd_ID_valid, self.rd_scan.ports.ID_out_valid)
 
-        self.wire(self.buffet.ports.rd_rsp_data, rd_scan.ports.rd_rsp_data_in)
-        self.wire(self.buffet.ports.rd_rsp_data_ready, rd_scan.ports.rd_rsp_data_in_ready)
-        self.wire(self.buffet.ports.rd_rsp_data_valid, rd_scan.ports.rd_rsp_data_in_valid)
+        self.wire(self.buffet.ports.rd_rsp_data, self.rd_scan.ports.rd_rsp_data_in)
+        self.wire(self.buffet.ports.rd_rsp_data_ready, self.rd_scan.ports.rd_rsp_data_in_ready)
+        self.wire(self.buffet.ports.rd_rsp_data_valid, self.rd_scan.ports.rd_rsp_data_in_valid)
 
-        self.wire(self._wr_scan_data_in, wr_scan.ports.data_in)
-        self.wire(self._wr_scan_data_in_ready, wr_scan.ports.data_in_ready)
-        self.wire(self._wr_scan_data_in_valid, wr_scan.ports.data_in_valid)
+        self.wire(self._wr_scan_data_in, self.wr_scan.ports.data_in)
+        self.wire(self._wr_scan_data_in_ready, self.wr_scan.ports.data_in_ready)
+        self.wire(self._wr_scan_data_in_valid, self.wr_scan.ports.data_in_valid)
 
-        self.wire(self._wr_scan_addr_in, wr_scan.ports.addr_in)
-        self.wire(self._wr_scan_addr_in_ready, wr_scan.ports.addr_in_ready)
-        self.wire(self._wr_scan_addr_in_valid, wr_scan.ports.addr_in_valid)
+        self.wire(self._wr_scan_addr_in, self.wr_scan.ports.addr_in)
+        self.wire(self._wr_scan_addr_in_ready, self.wr_scan.ports.addr_in_ready)
+        self.wire(self._wr_scan_addr_in_valid, self.wr_scan.ports.addr_in_valid)
 
-        self.wire(self._rd_scan_coord_out, rd_scan.ports.coord_out)
-        self.wire(self._rd_scan_coord_out_ready, rd_scan.ports.coord_out_ready)
-        self.wire(self._rd_scan_coord_out_valid, rd_scan.ports.coord_out_valid)
+        self.wire(self._rd_scan_coord_out, self.rd_scan.ports.coord_out)
+        self.wire(self._rd_scan_coord_out_ready, self.rd_scan.ports.coord_out_ready)
+        self.wire(self._rd_scan_coord_out_valid, self.rd_scan.ports.coord_out_valid)
 
-        self.wire(self._rd_scan_pos_out, rd_scan.ports.pos_out)
-        self.wire(self._rd_scan_pos_out_ready, rd_scan.ports.pos_out_ready)
-        self.wire(self._rd_scan_pos_out_valid, rd_scan.ports.pos_out_valid)
+        self.wire(self._rd_scan_pos_out, self.rd_scan.ports.pos_out)
+        self.wire(self._rd_scan_pos_out_ready, self.rd_scan.ports.pos_out_ready)
+        self.wire(self._rd_scan_pos_out_valid, self.rd_scan.ports.pos_out_valid)
 
-        self.wire(self._rd_scan_us_pos_in, rd_scan.ports.us_pos_in)
-        self.wire(self._rd_scan_us_pos_in_ready, rd_scan.ports.us_pos_in_ready)
-        self.wire(self._rd_scan_us_pos_in_valid, rd_scan.ports.us_pos_in_valid)
+        self.wire(self._rd_scan_us_pos_in, self.rd_scan.ports.us_pos_in)
+        self.wire(self._rd_scan_us_pos_in_ready, self.rd_scan.ports.us_pos_in_ready)
+        self.wire(self._rd_scan_us_pos_in_valid, self.rd_scan.ports.us_pos_in_valid)
 
         if self.add_clk_enable:
             # self.clock_en("clk_en")
@@ -181,9 +202,46 @@ class FiberAccess(MemoryController):
     def get_config_mode_str(self):
         return "fiber_access"
 
+    def get_bitstream(self, config_kwargs):
+
+        print("IN FLAVOR TOWN!!!")
+
+        assert 'flavor' in config_kwargs
+        flavor = config_kwargs['flavor']
+
+        if flavor == "read_scanner":
+            config = self.rd_scan.get_bitstream(config_kwargs=config_kwargs)
+        elif flavor == "write_scanner":
+            config = self.wr_scan.get_bitstream(config_kwargs=config_kwargs)
+        elif flavor == "buffet":
+            config = self.buffet.get_bitstream(config_kwargs=config_kwargs)
+
+        print(config)
+        for i, (name, val) in enumerate(config):
+            config[i] = (f"{flavor}_{name}", val)
+        print(config)
+
+        config += [("tile_en", 1)]
+
+        # stop_lvl = config_kwargs['stop_lvl']
+        # root = config_kwargs['root']
+        # if 'spacc_mode' in config_kwargs:
+        #         spacc_mode = config_kwargs['spacc_mode']
+
+        # # Store all configurations here
+        # config = [("tile_en", 1),
+        #           ("stop_lvl", stop_lvl),
+        #           ("root", root),
+        #           ("spacc_mode", spacc_mode)
+        #           ]
+
+        return config
+
 
 if __name__ == "__main__":
-    fiber_access_dut = FiberAccess(data_width=16)
+    fiber_access_dut = FiberAccess(data_width=16,
+                                   defer_fifos=False,
+                                   use_pipelined_scanner=True)
 
     # Lift config regs and generate annotation
     # lift_config_reg(pond_dut.internal_generator)
