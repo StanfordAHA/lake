41ceb2cf1b94852561bd8bc7f89230f263f74efd
steveri <steveri@steveri.com>
2023-06-09 17:29:46 -0700

    install py, verilator

--- .github/scripts/run.sh
+++ .github/scripts/run.sh
@@ -9,6 +9,13 @@ source scripts/setenv.sh
 # force color
 export PYTEST_ADDOPTS="--color=yes"
 
+pip install py
+apt-get update
+apt-get install verilator
+which verilator
+
+
+
 # python3 -m pycodestyle lake/
 # python3 -m pycodestyle tests/
 pytest -v tests/
