208f4c4d0b24320c2a9fc6dc02fab0ef1702ee53
joejoedesu <joekuaxie@gmail.com>
2023-07-21 23:08:12 -0700

    style fix?

--- SPARSE_UNIT_TEST/unit_test_fsdb.tcl
+++ SPARSE_UNIT_TEST/unit_test_fsdb.tcl
@@ -1,6 +1,6 @@
 dump -file cgra.fsdb -type FSDB
-dump -add unioner_tb -fsdb_opt +mda+packedmda+struct
-power unioner_tb.dut
+dump -add repeat_tb -fsdb_opt +mda+packedmda+struct
+power repeat_tb.dut
 power -enable
 run
 power -disable
