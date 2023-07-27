4f1a02b83bf3eaa6e99dac161a4357b1684fdc86
joejoedesu <joekuaxie@gmail.com>
2023-07-27 16:26:09 -0700

    debugging coord_drop

---
+++ SPARSE_UNIT_TEST/test_crd_drop_unit.py
@@ -0,0 +1,289 @@
+from lake.modules.crddrop import *
+import magma as m
+import kratos as k
+
+
+import sparse_helper
+from sparse_helper import convert_stream_to_onyx_interp
+from sam.sim.src.base import remove_emptystr
+from sam.sim.src.crd_manager import CrdDrop as CrdDrop_sim
+from sam.sim.test.test import TIMEOUT
+
+
+import subprocess
+import os
+import random
+random.seed(15)
+import string
+
+
+def init_module():
+    dut = CrdDrop(data_width=16,
+                    fifo_depth=2,
+                    lift_config=False,
+                    defer_fifos=False,
+                    add_flush=True)
+    # magma_dut = k.util.to_magma(dut, flatten_array=False, check_flip_flop_always_ff=True)
+    k.verilog(dut, filename=f"./modules/CrdDrop.sv",
+            optimize_if=False)
+    sparse_helper.update_tcl("coord_drop_tb")
+
+def create_random_fiber(rate, size, d, f_type = "coord"):
+    # size = int(size*random.uniform(1.0, 1.0+d))
+    s = int(rate*size)
+    if f_type == "coord":
+        crd = random.sample(range(size), s)
+        crd.sort()
+        return crd
+    elif f_type == "pos":
+        pos = random.sample(range(1, size + 1), s)
+        pos.sort()
+        return pos
+    else:
+        val = [random.uniform(0, 2**15-1) for i in range(s)]
+        return val
+
+def create_random(n, rate, size, d1=0):
+    if n == 1:
+        in_crd1 = create_random_fiber(rate, size, 0.2, "coord") + ['S0', 'D']
+        in_ref1 = create_random_fiber(1, len(in_crd1) - 2, 0, "pos") + ['S0', 'D']
+        in_crd2 = create_random_fiber(rate, size, 0.2, "coord") + ['S0', 'D']
+        in_ref2 = create_random_fiber(1, len(in_crd2) - 2, 0, "pos") + ['S0', 'D']
+        return in_crd1, in_crd2, in_ref1, in_ref2
+    
+    elif n == 2:
+        if d1 == 0:
+            d1 = int(random.uniform(2, int(size**(1/2))))
+        d2 = size // d1
+        rate = rate**(1/2)
+        ret = [[] for i in range(4)]
+        for i in range(d1):
+            ret_t = [[] for j in range(4)]
+            if random.random() < rate:
+                ret_t[0] = create_random_fiber(rate, d2, 0.2, "coord")
+                ret_t[1] = create_random_fiber(1, len(ret_t[0]), 0, "pos")
+            if random.random() < rate:
+                ret_t[2] = create_random_fiber(rate, d2, 0.2, "coord")
+                ret_t[3] = create_random_fiber(1, len(ret_t[2]), 0, "pos")
+
+            for j in range(4):
+                if i == d1 - 1:
+                    ret[j] = ret[j] + ret_t[j] + ['S1', 'D']
+                else:
+                    ret[j] = ret[j] + ret_t[j] + ['S0']
+        return ret[0], ret[1], ret[2], ret[3]
+    elif n == 3:
+        if d1 == 0:
+            d1 = int(random.uniform(2, int(size**(1/3))))
+        d2 = size // d1
+        d1_ = int(random.uniform(2, int(d2**(1/2))))
+        rate = rate**(1/3)
+        ret = [[] for i in range(4)]
+        # print(d1, d2, rate)
+        for i in range(d1):
+            # print("i---------: ", i)
+            ret_t = [[] for j in range(4)]
+            if random.random() < rate:
+                ret_t[0], ret_t[1], ret_t[2], ret_t[3] = create_random(2, rate*rate, d2, d1_)
+            else:
+                for j in range(4):
+                    ret_t[j] = ret_t[j] + ['S1', 'D']
+            
+            for j in range(4):
+                if i == d1 - 1:
+                    ret[j] = ret[j] + ret_t[j][:-2] + ['S2', 'D']
+                else:
+                    ret[j] = ret[j] + ret_t[j][:-1]
+                # print(j)
+                # print(ret_t[j][:-1])
+                # print(ret[j])
+        return ret[0], ret[1], ret[2], ret[3]
+
+
+def remove_emptyfiber(out_in):
+    r = []
+    for i in range(len(out_in)):
+        if sparse_helper.is_STOP_sam(out_in[i]) and sparse_helper.is_STOP_sam(out_in[i + 1]): 
+            continue
+        r.append(out_in[i])
+    return r
+
+def create_gold(in_crd1, in_crd2): # 1 is outer, 0 is inner
+    if len([x for x in in_crd1 if type(x) is int]) == 0:
+        assert len([x for x in in_crd2 if sparse_helper.is_STOP_sam(x)]) == 1
+    else:
+        assert (len([x for x in in_crd1 if type(x) is int]) == \
+            len([x for x in in_crd2 if sparse_helper.is_STOP_sam(x)]))
+    
+    i_c1_cpy = in_crd1[:]
+    i_c2_cpy = in_crd2[:]
+
+    done = False
+    time = 0
+
+    cd = CrdDrop_sim()
+    out_outer = []
+    out_inner = []
+    while not done and time < TIMEOUT:
+        if len(in_crd2) > 0:
+            cd.set_inner_crd(in_crd2.pop(0))
+        if len(in_crd1) > 0:
+            cd.set_outer_crd(in_crd1.pop(0))
+
+        cd.update()
+
+        out_outer.append(cd.out_crd_outer())
+        out_inner.append(cd.out_crd_inner())
+
+        # print("Timestep", time, "\t Done:", cd.out_done(), "\t Out:", cd.out_crd_outer())
+
+        done = cd.out_done()
+        time += 1
+
+    out_outer = remove_emptystr(out_outer)
+    out_inner = remove_emptystr(out_inner)
+
+    out_inner = remove_emptyfiber(out_inner) # in another sam module, but cannot find it
+
+    print("sam cycle count: ", time)
+
+    st = [i_c1_cpy, i_c2_cpy, out_outer, out_inner]
+    tr_st = []
+    for s in st:
+        tr_st.append(convert_stream_to_onyx_interp(s))
+
+    return tr_st
+
+
+def load_test_module(test_name):
+    if test_name == "stream_1":
+        in_crd_o = [0, 1, 'S0', 'D']
+        in_crd_i = [1, 'S0', 'S1', 'D']
+
+        return create_gold(in_crd_o, in_crd_i)
+
+    elif test_name == "stream_2":
+        in_crd_o = [0, 1, 'S0', 'D']
+        in_crd_i =  [1, 'S0', 1, 'S1', 'D']
+
+        return create_gold(in_crd_o, in_crd_i)
+
+    elif test_name == "stream_3":
+        in_crd_o = [0, 1, 2, 3, 'S0', 'D']
+        in_crd_i = [1, 'S0', 1, 'S0', 'S0', 1, 'S1', 'D']
+
+        return create_gold(in_crd_o, in_crd_i)
+
+    elif test_name == "stream_4":
+        in_crd_o = [1, 'S0', 'D']
+        in_crd_i = [1, 2, 'S1', 'D']
+
+        return create_gold(in_crd_o, in_crd_i)
+
+    elif test_name[0:3] == "rd_":
+        t_arg = test_name.split("_")
+        n = int(t_arg[1][0])
+        rate = float(t_arg[2])
+        size = int(t_arg[3])
+        [in_crd_o, in_ref1, in_crd_i, in_ref2] = create_random(n, rate, size)
+        return create_gold(in_crd_o, in_crd_i)
+
+    else:
+        in_crd_o = ['S0', 'D']
+        in_crd_i = ['S1', 'D']
+
+        return create_gold(in_crd_o, in_crd_i)
+
+
+def module_iter_basic(test_name, add_test=""):
+    [ic1, ic2, gc1, gc2] = load_test_module(test_name)
+    if add_test != "":
+        additional_t = load_test_module(add_test)
+        ic1 = ic1 + additional_t[0]
+        ic2 = ic2 + additional_t[1]
+        gc1 = gc1 + additional_t[2]
+        gc2 = gc2 + additional_t[3]
+
+    print("ic1", ic1)
+    print("ic2", ic2)
+    print("gc1", gc1)
+    print("gc2", gc2)
+
+    sparse_helper.write_txt("coord_in_0.txt", ic1)
+    sparse_helper.write_txt("coord_in_1.txt", ic2)
+
+    sparse_helper.clear_txt("pos_out_0.txt")
+    sparse_helper.clear_txt("pos_out_1.txt") 
+    
+    #run command "make sim" to run the simulation
+    if add_test == "":
+        sim_result = subprocess.run(["make", "sim", "TEST_TAR=coord_drop_tb.sv", "TOP=coord_drop_tb",\
+                             "TEST_UNIT=CrdDrop.sv"], capture_output=True, text=True)
+    else:
+        sim_result = subprocess.run(["make", "sim", "TEST_TAR=coord_drop_tb.sv",\
+                             "TOP=coord_drop_tb", "TX_NUM_GLB=2", "TEST_UNIT=CrdDrop.sv"\
+                             ], capture_output=True, text=True)
+    output = sim_result.stdout
+    # print(output)
+    cycle_count_line = output[output.find("cycle count:"):]
+    print(cycle_count_line.splitlines()[0])
+
+    pos_out_1 = sparse_helper.read_txt("pos_out_0.txt", addit=add_test != "")
+    pos_out_0 = sparse_helper.read_txt("pos_out_1.txt", addit=add_test != "")
+
+    print(pos_out_0)
+    print(pos_out_1)
+    
+    #compare each element in the output from pos_out_0.txt with the gold output
+    assert len(pos_out_0) == len(gc1), \
+        f"Output length {len(pos_out_0)} didn't match gold length {len(gc1)}"
+    for i in range(len(pos_out_0)):
+        assert pos_out_0[i] == gc1[i], \
+            f"Output {pos_out_0[i]} didn't match gold {gc1[i]} at index {i}"
+    
+    #compare each element in the output from pos_out_1.txt with the gold output
+    assert len(pos_out_1) == len(gc2), \
+        f"Output length {len(pos_out_1)} didn't match gold length {len(gc2)}"
+    for i in range(len(pos_out_1)):
+        assert pos_out_1[i] == gc2[i], \
+            f"Output {pos_out_1[i]} didn't match gold {gc2[i]} at index {i}"
+    
+    print(test_name, " passed\n")
+
+
+def test_iter_basic():
+    init_module()
+    test_list = ["stream_1", "stream_2", "stream_3", "stream_4", "xxx"]
+    for test in test_list:
+        module_iter_basic(test)
+
+
+# def test_random_1d():
+#     init_module()
+#     test_list = ["rd_1d_0.1_400", "rd_1d_0.3_400", "rd_1d_0.5_400", "rd_1d_0.8_400", "rd_1d_1.0_400"]
+#     for test in test_list:
+#         module_iter_basic(test)
+
+
+# def test_random_2d():
+#     init_module()
+#     test_list = ["rd_2d_0.1_400", "rd_2d_0.3_400", "rd_2d_0.5_400", "rd_2d_0.8_400", "rd_2d_1.0_400"]
+#     for test in test_list:
+#         module_iter_basic(test)
+
+
+# def test_random_3d():
+#     init_module()
+#     test_list = ["rd_3d_0.1_400", "rd_3d_0.3_400", "rd_3d_0.5_400", "rd_3d_0.8_400", "rd_3d_1.0_400"]
+#     for test in test_list:
+#         module_iter_basic(test) 
+
+
+# def test_seq():
+#     init_module()
+#     test_list =  ["rd_1d_0.1_400", "rd_1d_0.3_400", "rd_1d_0.5_400", "rd_1d_0.8_400", "rd_1d_1.0_400"] +\
+#                  ["rd_2d_0.1_400", "rd_2d_0.3_400", "rd_2d_0.5_400", "rd_2d_0.8_400", "rd_1d_1.0_400"] +\
+#                  ["rd_3d_0.1_400", "rd_3d_0.3_400", "rd_3d_0.5_400", "rd_3d_0.8_400", "rd_1d_1.0_400"]
+#     for i in range(10):
+#         rand = random.sample(test_list, 2)
+#         module_iter_basic(rand[0], rand[1])
--- SPARSE_UNIT_TEST/unit_test_fsdb.tcl
+++ SPARSE_UNIT_TEST/unit_test_fsdb.tcl
@@ -1,6 +1,6 @@
 dump -file cgra.fsdb -type FSDB
-dump -add reduce_tb -fsdb_opt +mda+packedmda+struct
-power reduce_tb.dut
+dump -add coord_drop_tb -fsdb_opt +mda+packedmda+struct
+power coord_drop_tb.dut
 power -enable
 run
 power -disable
