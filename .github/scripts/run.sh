f073ecf2303984c08fa265d81df3d3c2deb52fdd
steveri <steveri@steveri.com>
2024-04-13 09:01:20 -0700

    assuming pytests do not actually use verilator...okay...?

--- .github/scripts/run.sh
+++ .github/scripts/run.sh
@@ -9,10 +9,11 @@ source scripts/setenv.sh
 # force color
 export PYTEST_ADDOPTS="--color=yes"
 
-echo pip install py, apt-get install verilator
-pip install py
-apt-get update
-apt-get install verilator
+# Do we really need these?
+# echo pip install py, apt-get install verilator
+# pip install py
+# apt-get update
+# apt-get install verilator
 
 echo python3 -m pycodestyle lake/
 python3 -m pycodestyle lake/
