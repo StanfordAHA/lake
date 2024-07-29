258ceab25b018d06c27d9380dd2063de7dcc818f
Max Strange <mstrange@stanford.edu>
2024-07-29 13:24:24 -0700

    add quad port with 2-wide dual port

--- tests/test_spec/dual_port_wide_fetch_quad.py
+++ tests/test_spec/dual_port_wide_fetch_quad.py
@@ -12,8 +12,8 @@ import argparse
 import os
 
 
-def build_dual_port_wide_fetch(storage_capacity=1024, data_width=16, dims: int = 6,
-                               vec_width=4, physical=False, reg_file=False) -> Spec:
+def build_dual_port_wide_fetch_quad(storage_capacity=1024, data_width=16, dims: int = 6,
+                                    vec_width=2, physical=False, reg_file=False) -> Spec:
 
     read_delay = 0 if reg_file else 1
 
@@ -23,19 +23,32 @@ def build_dual_port_wide_fetch(storage_capacity=1024, data_width=16, dims: int =
                    vec_capacity=8, runtime=Runtime.STATIC, direction=Direction.IN)
     out_port = Port(ext_data_width=data_width, int_data_width=data_width * vec_width,
                     vec_capacity=8, runtime=Runtime.STATIC, direction=Direction.OUT)
+    in_port2 = Port(ext_data_width=data_width, int_data_width=data_width * vec_width,
+                    vec_capacity=8, runtime=Runtime.STATIC, direction=Direction.IN)
+    out_port2 = Port(ext_data_width=data_width, int_data_width=data_width * vec_width,
+                     vec_capacity=8, runtime=Runtime.STATIC, direction=Direction.OUT)
 
     ls.register(in_port, out_port)
+    ls.register(in_port2, out_port2)
 
     in_id = IterationDomain(dimensionality=dims, extent_width=16)
     in_ag = AddressGenerator(dimensionality=dims)
     in_sg = ScheduleGenerator(dimensionality=dims)
+    in_id2 = IterationDomain(dimensionality=dims, extent_width=16)
+    in_ag2 = AddressGenerator(dimensionality=dims)
+    in_sg2 = ScheduleGenerator(dimensionality=dims)
 
     out_id = IterationDomain(dimensionality=dims, extent_width=16)
     out_ag = AddressGenerator(dimensionality=dims)
     out_sg = ScheduleGenerator(dimensionality=dims)
+    out_id2 = IterationDomain(dimensionality=dims, extent_width=16)
+    out_ag2 = AddressGenerator(dimensionality=dims)
+    out_sg2 = ScheduleGenerator(dimensionality=dims)
 
     ls.register(in_id, in_ag, in_sg)
+    ls.register(in_id2, in_ag2, in_sg2)
     ls.register(out_id, out_ag, out_sg)
+    ls.register(out_id2, out_ag2, out_sg2)
 
     data_bytes = (data_width * vec_width) // 8
     tech_map = None
@@ -56,15 +69,23 @@ def build_dual_port_wide_fetch(storage_capacity=1024, data_width=16, dims: int =
     ls.connect(in_port, in_id)
     ls.connect(in_port, in_ag)
     ls.connect(in_port, in_sg)
+    ls.connect(in_port2, in_id2)
+    ls.connect(in_port2, in_ag2)
+    ls.connect(in_port2, in_sg2)
 
     # Out to out
     ls.connect(out_port, out_id)
     ls.connect(out_port, out_ag)
     ls.connect(out_port, out_sg)
+    ls.connect(out_port2, out_id2)
+    ls.connect(out_port2, out_ag2)
+    ls.connect(out_port2, out_sg2)
 
     # In and Out to memory ports
     ls.connect(in_port, wr_mem_port)
+    ls.connect(in_port2, wr_mem_port)
     ls.connect(out_port, rd_mem_port)
+    ls.connect(out_port2, rd_mem_port)
 
     # Memory Ports to storage
     ls.connect(wr_mem_port, stg)
@@ -118,7 +139,7 @@ def get_linear_test():
         }
     }
 
-    linear_test[1] = {
+    linear_test[2] = {
         'type': Direction.OUT,
         'name': 'read_port_0',
         'config': {
@@ -162,8 +183,8 @@ def get_linear_test():
     return linear_test
 
 
-def test_linear_read_write_dp_wf(output_dir=None, storage_capacity=1024, data_width=16, physical=False, vec_width=4,
-                                 tp: TestPrepper = None, reg_file=False):
+def test_linear_read_write_dp_wf_q(output_dir=None, storage_capacity=1024, data_width=16, physical=False, vec_width=4,
+                                   tp: TestPrepper = None, reg_file=False):
 
     assert tp is not None
 
@@ -176,8 +197,8 @@ def test_linear_read_write_dp_wf(output_dir=None, storage_capacity=1024, data_wi
 
     print(f"putting verilog at {output_dir_verilog}")
     # Build the spec
-    simple_single_port_spec = build_dual_port_wide_fetch(storage_capacity=storage_capacity, data_width=data_width,
-                                                         physical=physical, vec_width=vec_width, reg_file=reg_file)
+    simple_single_port_spec = build_dual_port_wide_fetch_quad(storage_capacity=storage_capacity, data_width=data_width,
+                                                              physical=physical, vec_width=vec_width, reg_file=reg_file)
     simple_single_port_spec.visualize_graph()
     simple_single_port_spec.generate_hardware()
     simple_single_port_spec.extract_compiler_information()
@@ -231,7 +252,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Simple Dual Port')
     parser.add_argument("--storage_capacity", type=int, default=1024)
     parser.add_argument("--data_width", type=int, default=16)
-    parser.add_argument("--vec_width", type=int, default=4)
+    parser.add_argument("--vec_width", type=int, default=2)
     parser.add_argument("--reg_file", action="store_true")
     parser.add_argument("--clock_count_width", type=int, default=64)
     parser.add_argument("--tech", type=str, default="GF")
@@ -247,5 +268,5 @@ if __name__ == "__main__":
     hw_test_dir = tp.prepare_hw_test()
     print(f"Put hw test at {hw_test_dir}")
 
-    test_linear_read_write_dp_wf(output_dir=hw_test_dir, storage_capacity=args.storage_capacity, data_width=args.data_width,
-                                 physical=args.physical, vec_width=args.vec_width, tp=tp, reg_file=args.reg_file)
+    test_linear_read_write_dp_wf_q(output_dir=hw_test_dir, storage_capacity=args.storage_capacity, data_width=args.data_width,
+                                   physical=args.physical, vec_width=args.vec_width, tp=tp, reg_file=args.reg_file)
