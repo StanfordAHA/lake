1fa99ca949dd43f4a6ade7bca6be4f388575055e
yuchen-mei <yuchen.mei@outlook.com>
2025-05-07 15:10:09 -0700

    fix PE output valid assertion logic when removing the primitive output fifo to avoid deadlock

--- lake/modules/onyx_pe.py
+++ lake/modules/onyx_pe.py
@@ -144,6 +144,7 @@ class OnyxPE(MemoryController):
         # Some local vars
         self.all_active_inputs_valid = self.var("all_active_inputs_valid", 1)
         self.all_active_bit_inputs_valid = self.var("all_active_bit_inputs_valid", 1)
+        self.output_rv_transaction_ack = self.var("output_rv_transaction_ack", 1)
 
         if self.perf_debug:
 
@@ -397,6 +398,7 @@ class OnyxPE(MemoryController):
             self._infifo_pop[0] = 0
             self._infifo_pop[1] = 0
             self._infifo_pop[2] = 0
+            self.output_rv_transaction_ack = 0
 
             self._outfifo_bit_push = 0
             self._bit_to_fifo = 0
@@ -414,19 +416,19 @@ class OnyxPE(MemoryController):
 
             else:
                 # TODO Fix comment If both inputs are valid, then we either can perform the op, otherwise we push through EOS
-                if (self.all_active_inputs_valid & ~self._outfifo_full &
-                        self.all_active_bit_inputs_valid & ~self._outfifo_bit_full & ~self._bypass_rv_mode):
+                if (self.all_active_inputs_valid & self.all_active_bit_inputs_valid & ~self._outfifo_bit_full & ~self._bypass_rv_mode):
                     # if eos's are low, we push through pe output, otherwise we push through the input data (streams are aligned)
                     # go through here if any of the 1b inputs or outputs are there - implies not sparse
+                    self.output_rv_transaction_ack = self._active_16b_output & ~self._outfifo_full
                     if ~((self._infifo_out_eos & self._active_inputs_encoding) == self._active_inputs_encoding) | (self._active_1b_output | (self._active_bit_inputs_encoding.r_or())):
 
                         # Push to 16/17b FIFO if that output is active
                         self._outfifo_push = self._active_16b_output
                         self._outfifo_in_eos = 0
                         self._data_to_fifo = self._pe_output
-                        self._infifo_pop[0] = self._infifo_out_valid[0] & self._active_inputs_encoding[0]
-                        self._infifo_pop[1] = self._infifo_out_valid[1] & self._active_inputs_encoding[1]
-                        self._infifo_pop[2] = self._infifo_out_valid[2] & self._active_inputs_encoding[2]
+                        self._infifo_pop[0] = self._infifo_out_valid[0] & self._active_inputs_encoding[0] & self.output_rv_transaction_ack
+                        self._infifo_pop[1] = self._infifo_out_valid[1] & self._active_inputs_encoding[1] & self.output_rv_transaction_ack
+                        self._infifo_pop[2] = self._infifo_out_valid[2] & self._active_inputs_encoding[2] & self.output_rv_transaction_ack
 
                         # Push to 1b FIFO if that output is active
                         self._outfifo_bit_push = self._active_1b_output
@@ -440,9 +442,9 @@ class OnyxPE(MemoryController):
                         self._outfifo_in_eos = 1
                         # TODO what if stream not on first input
                         self._data_to_fifo = kts.ternary(self._active_inputs_encoding[0], self._infifo_out_data[0], kts.ternary(self._active_inputs_encoding[1], self._infifo_out_data[1], self._infifo_out_data[2]))
-                        self._infifo_pop[0] = self._infifo_out_valid[0] & self._active_inputs_encoding[0]
-                        self._infifo_pop[1] = self._infifo_out_valid[1] & self._active_inputs_encoding[1]
-                        self._infifo_pop[2] = self._infifo_out_valid[2] & self._active_inputs_encoding[2]
+                        self._infifo_pop[0] = self._infifo_out_valid[0] & self._active_inputs_encoding[0] & self.output_rv_transaction_ack
+                        self._infifo_pop[1] = self._infifo_out_valid[1] & self._active_inputs_encoding[1] & self.output_rv_transaction_ack
+                        self._infifo_pop[2] = self._infifo_out_valid[2] & self._active_inputs_encoding[2] & self.output_rv_transaction_ack
 
         self.add_code(fifo_push)
 
