a7c4d88634d0cd165c2500422d8fb2130985355c
yuchen-mei <yuchen.mei@outlook.com>
2025-05-09 13:50:41 -0700

    fix pe prim outfifo push logic for rv accum; fix rv config for vec accum

--- lake/spec/hack_rv_mem_pond_bitstream.py
+++ lake/spec/hack_rv_mem_pond_bitstream.py
@@ -4,6 +4,7 @@ import os
 
 APPS_NEEDING_HACKS = [
     "scalar_reduction_fp",
+    "vector_reduction_fp"
 ]
 
 
@@ -28,6 +29,14 @@ def hack_rv_config(test_name):
         num_partial_reduction = vec_len // int(halide_gen_args_dict['glb_i'])
         rv_config = get_accum_pond(num_partial_reduction=num_partial_reduction,
                                    num_output_pixels=1)
+    elif test_name == "vector_reduction_fp":
+        # Only have one Pond
+        # "HALIDE_GEN_ARGS" example: "vec_width=256 vec_height=2 glb_i=8 glb_o=1 tree_stages=3"
+        vec_len = int(halide_gen_args_dict['vec_width'])
+        num_vecs = int(halide_gen_args_dict['vec_height'])
+        num_partial_reduction = vec_len // int(halide_gen_args_dict['glb_i'])
+        rv_config = get_vec_accum_pond(num_partial_reduction=num_partial_reduction,
+                                   num_output_pixels=num_vecs)
 
     assert rv_config, f"rv_config is empty for test_name: {test_name}"
     return rv_config
@@ -36,6 +45,8 @@ def hack_rv_config(test_name):
 def get_accum_pond(num_partial_reduction=64, num_output_pixels=1):
     '''
     Helper function to create config for reduction pond
+    Pond port mapping: 0: port_w0, 1: port_init (clear memory) 2: port_r0, 3: port_r1
+    MEM port mapping: 0: port_w0, 1: port_w1, 2: port_r0, 3: port_r1
     '''
 
     linear_test = {}
@@ -58,7 +69,7 @@ def get_accum_pond(num_partial_reduction=64, num_output_pixels=1):
         'vec_constraints': []
     }
 
-    linear_test[1] = {
+    linear_test[2] = {
         'name': 'port_r0',
         'type': Direction.OUT,
         'config': {
@@ -76,7 +87,7 @@ def get_accum_pond(num_partial_reduction=64, num_output_pixels=1):
         'vec_constraints': []
     }
 
-    linear_test[2] = {
+    linear_test[3] = {
         'name': 'port_r1',
         'type': Direction.OUT,
         'config': {
@@ -95,8 +106,8 @@ def get_accum_pond(num_partial_reduction=64, num_output_pixels=1):
     }
 
     pw = 0
-    pr1 = 1
-    pr2 = 2
+    pr1 = 2
+    pr2 = 3
 
     pr1_raw_idx = 0
     pw1_raw_idx = 0
@@ -113,5 +124,113 @@ def get_accum_pond(num_partial_reduction=64, num_output_pixels=1):
 
     # Just have read follow write
     linear_test['constraints'] = [raw_r1_constraint, raw_r2_constraint]
+
+    return linear_test
+
+def get_vec_accum_pond(num_partial_reduction=64, num_output_pixels=4):
+    '''
+    Helper function to create config for vector reduction pond, where pond has to autorestart after each vector's reduction
+    Pond port mapping: 0: port_w0, 1: port_init (clear memory) 2: port_r0, 3: port_r1
+    MEM port mapping: 0: port_w0, 1: port_w1, 2: port_r0, 3: port_r1
+    '''
+
+    linear_test = {}
+
+    linear_test[0] = {
+        'name': 'port_w0',
+        'type': Direction.IN,
+        'config': {
+            'dimensionality': 2,
+            'extents': [num_partial_reduction, num_output_pixels],
+            'address': {
+                # read-modify-write the same address
+                'strides': [0, 0],
+                'offset': 0
+            },
+            'schedule': {}
+        },
+        'vec_in_config': {},
+        'vec_out_config': {},
+        'vec_constraints': []
+    }
+
+    linear_test[1] = {
+        'name': 'port_w_init',
+        'type': Direction.IN,
+        'config': {
+            'dimensionality': 1,
+            'extents': [num_output_pixels],
+            'address': {
+                # address for the init (flush) port does not matter and it's not consumed
+                # this port just OR in the 1 bit enable into the flush
+                'strides': [0],
+                'offset': 0
+            },
+            'schedule': {}
+        },
+        'vec_in_config': {},
+        'vec_out_config': {},
+        'vec_constraints': []
+    }
+
+    linear_test[2] = {
+        'name': 'port_r0',
+        'type': Direction.OUT,
+        'config': {
+            'dimensionality': 2,
+            'extents': [num_partial_reduction, num_output_pixels],
+            'address': {
+                # read-modify-write the same address
+                'strides': [0, 0],
+                'offset': 0
+            },
+            'schedule': {}
+        },
+        'vec_in_config': {},
+        'vec_out_config': {},
+        'vec_constraints': []
+    }
+
+    linear_test[3] = {
+        'name': 'port_r1',
+        'type': Direction.OUT,
+        'config': {
+            'dimensionality': 1,
+            'extents': [num_output_pixels],
+            'address': {
+                # read-modify-write the same address
+                'strides': [0],
+                'offset': 0
+            },
+            'schedule': {}
+        },
+        'vec_in_config': {},
+        'vec_out_config': {},
+        'vec_constraints': []
+    }
+
+    pw = 0
+    pwinit = 1
+    pr1 = 2
+    pr2 = 3
+
+    # Let the update port read out a data before write update
+    raw_r1_scalar = -1
+    raw_r1_constraint = (pr1, 0, pw, 0, LFComparisonOperator.LT.value, raw_r1_scalar)
+
+    # Wait for the write update to finish its last accum write before spill
+    raw_r2_scalar = 0
+    raw_r2_constraint = (pr2, 0, pw, 1, LFComparisonOperator.LT.value, raw_r2_scalar)
+
+    # Don't let the read_update start until the memory has been cleared
+    raw_r1_pwinit_scalar = 0
+    raw_r1_pwinit_constraint = (pr1, 1, pwinit, 0, LFComparisonOperator.LT.value, raw_r1_pwinit_scalar)
+
+    # Don't clear the memory until the spill has happened (slip of 1)
+    war_pwinit_r2_scalar = 1
+    war_winit_r2_constraint = (pwinit, 0, pr2, 0, LFComparisonOperator.GT.value, war_pwinit_r2_scalar)
+
+    # Add all constraints
+    linear_test['constraints'] = [raw_r1_constraint, raw_r2_constraint, raw_r1_pwinit_constraint, war_winit_r2_constraint]
 
     return linear_test
