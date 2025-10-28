2ce88a2ae168fb78f4559263ca055448ec202fd7
steveri <steveri@steveri.com>
2025-10-28 11:54:27 -0700

    fix it again

--- run_lake.py
+++ run_lake.py
@@ -1,1 +1,12 @@
-print("--- hey look at me, me so stoopid now")
+import kratos
+from lake.modules.passthru import *
+from lake.top.lake_top import *
+from lake.modules.sram_stub import *
+
+# lake_top = LakeTop(16)
+# lake_top_src = kratos.verilog(lake_top, optimize_passthrough=False)
+# print(lake_top_src["LakeTop"])
+
+sramstub = SRAMStub(16, 512)
+sramstub_src = kratos.verilog(sramstub, optimize_passthrough=False)
+print(sramstub_src["SRAMStub"])
